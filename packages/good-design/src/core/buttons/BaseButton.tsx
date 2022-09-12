8fe916fffe8a3c471a08578ecea6330983b72ef9
Lewis B <lewis@ikigaistudios.eu>
2022-09-13 00:02:56 +0200

    add: basic storybook config with nativebase

---
+++ packages/good-design/src/core/buttons/BaseButton.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { GooddollarThemeProvider } from '../provider/ThemeProvider'
+import { Button } from 'native-base';
+
+export type BaseButtonProps = {
+  /**
+   * a text to be rendered in the component.
+   */
+  text: string
+};
+
+
+export function BaseButton({ text }: BaseButtonProps) {
+  return (
+    <GooddollarThemeProvider>
+      <Button bg={"gooddollar.secondary"}>{text}</Button>
+    </GooddollarThemeProvider>
+  );
+}
+
+// const styles = StyleSheet.create({
+//   text: {},
+// });
+
+// bg={"gooddollar.secondary"}
