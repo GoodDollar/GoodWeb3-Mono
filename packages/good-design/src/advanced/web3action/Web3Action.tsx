b60ff3f79f85768bf185ff2c553602390b6b0650
Lewis B <lewis@ikigaistudios.eu>
2022-10-13 03:05:18 +0200

    draft: add web3action button

--- packages/good-design/src/advanced/web3action/Web3Action.tsx
+++ packages/good-design/src/advanced/web3action/Web3Action.tsx
@@ -1,57 +1,189 @@
 import React, { useCallback, useEffect, useState } from "react";
 import { noop } from "lodash";
-import { BaseButton } from "../../core/buttons";
-import { useEthers } from "@usedapp/core";
-import { TransactionReceipt } from "@ethersproject/providers";
+import { BaseButton, BaseButtonProps } from "../../core/buttons";
+import { useEthers, useNetwork } from "@usedapp/core";
+import { HStack, Spinner, Heading } from "native-base";
 
-//This is just a draft
-
-export interface Web3ActionProps {
+export interface Web3ActionProps extends BaseButtonProps {
   /**
    * a text to be rendered in the component.
    */
   text: string;
   requiredChain: number;
-  doAction: () => Promise<TransactionReceipt | undefined>;
+  web3Action: () => Promise<void> | void;
   switchChain?: () => Promise<any>;
   handleConnect?: (requiredChain: number) => Promise<any> | void;
 }
+
+const StepIndicator = ({ text }: { text?: string | undefined }) => {
+  return (
+    <HStack space={2} alignItems="center" flexDirection={"row"}>
+      <Spinner accessibilityLabel="Waiting on wallet confirmation" />
+      <Heading color="primary.500" fontSize="md">
+        {text}
+      </Heading>
+    </HStack>
+  );
+};
 
 export const Web3ActionButton = ({
   text,
   requiredChain,
   switchChain,
-  doAction,
+  web3Action,
   handleConnect = noop
 }: Web3ActionProps): JSX.Element => {
+  const _ = require("lodash");
+  // todo: add check on account between RO provider address / actual connected account
   const { account, switchNetwork, chainId, library, activateBrowserWallet } = useEthers();
-
+  const { network } = useNetwork();
   const [isPressed, setIsPressed] = useState<boolean>(false);
-  const [actionText, setActionText] = useState<string | undefined>(undefined);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [actionText, setActionText] = useState<string | undefined>();
+  const [baseText, setBaseText] = useState<string | undefined>(text);
+
+  const buttonSequence = [0, 1, 2];
+  const [steps, setSteps] = useState(_(buttonSequence));
+
+  useEffect(() => {
+    setBaseText(text);
+  }, [text]);
 
   useEffect(() => {
     if (isPressed) {
+      // todo: clear timeout function
       setTimeout(() => {
         setIsPressed(false);
-      }, 1000);
+        setBaseText(text);
+        setSteps(_(buttonSequence));
+      }, 60000);
     }
-  }, [account, chainId, library, isPressed]);
+  }, [account, chainId, library, isPressed, network]);
 
   const onPress = useCallback(async () => {
     setIsPressed(true);
-    console.log("account/onConnect -->", { account, handleConnect });
+    setBaseText(undefined);
+    continueSequence();
+  }, [account, chainId, library, steps]);
 
-    if (!account && !handleConnect) {
-      console.log("no account . . .");
-      console.log("library/account/chainId -->", { library, account, chainId });
-      activateBrowserWallet();
+  const resetButton = () => {
+    setIsLoading(false);
+    setBaseText(text);
+  };
+
+  const handleDefaultConnect = useCallback(async () => {
+    if (!account && handleConnect === noop) {
+      try {
+        await activateBrowserWallet();
+        steps.next();
+        setIsLoading(false);
+      } catch (e: any) {
+        if (e.code === 4001) {
+          // rejected
+          resetButton();
+        }
+        return e;
+      }
+    } else if (!account) {
+      // dapp provided connect
+      const dappConnected = await handleConnect(requiredChain);
+    } else {
+      steps.next();
+      setIsLoading(false);
+    }
+  }, [account, chainId, library]);
+
+  const handleSwitch = useCallback(async () => {
+    if (requiredChain === chainId) {
+      steps.next();
+      setIsLoading(false);
+    } else {
+      try {
+        // todo: add dapp provided switch
+        await switchNetwork(requiredChain);
+        steps.next();
+        setIsLoading(false);
+      } catch (e: any) {
+        console.log("e -->", { e });
+        if (e.code === 4001) {
+          resetButton();
+        }
+        return e;
+      }
+    }
+  }, [chainId, library]);
+
+  const handleAction = useCallback(async () => {
+    try {
+      await web3Action();
+    } catch (e: any) {
+      console.log("action failed -- e -->", { e });
+    }
+  }, [account, chainId, library]);
+
+  useEffect(() => {
+    if (!isLoading) {
+      setIsLoading(true);
+      if (!account) {
+        setSteps(_(buttonSequence));
+      } else if (steps.__index__ || steps.__index__ === 0) {
+        switch (steps.__index__) {
+          case 0:
+            steps.next();
+            if (requiredChain === chainId) {
+              steps.next();
+            }
+            break;
+          case 1:
+            if (requiredChain === chainId) {
+              steps.next();
+            }
+            break;
+        }
+      }
+      setIsLoading(false);
     }
 
-    if (chainId !== requiredChain) {
-      await switchNetwork(requiredChain);
-      return doAction();
+    if (isPressed && !isLoading) {
+      continueSequence();
     }
-  }, [account, chainId]);
+  }, [account, chainId, library, handleAction]);
 
-  return <BaseButton text={text} onPress={onPress} />;
+  const continueSequence = useCallback(async () => {
+    setIsLoading(true);
+    const currentStep = steps.__index__;
+    switch (currentStep) {
+      case 0:
+        if (!account) {
+          setActionText("Connecting wallet...");
+        } else {
+          steps.next();
+          break;
+        }
+        const isNotConnected = await handleDefaultConnect();
+        if (isNotConnected) {
+          setSteps(_(buttonSequence));
+        }
+        break;
+      case 1:
+        setActionText("Switching network...");
+        await handleSwitch();
+        break;
+      case 2:
+        setActionText("awaiting confirmation");
+        await handleAction();
+        resetButton();
+        setSteps(_(buttonSequence));
+        break;
+      default:
+        setSteps(_(buttonSequence));
+        break;
+    }
+  }, [account, chainId, steps]);
+
+  return (
+    <BaseButton text={baseText} onPress={onPress}>
+      {isPressed && isLoading && !baseText && <StepIndicator text={actionText} />}
+    </BaseButton>
+  );
 };
--- packages/good-design/src/stories/Web3Action.stories.tsx
+++ packages/good-design/src/stories/Web3Action.stories.tsx
@@ -1,39 +1,75 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useCallback, useState } from "react";
 import { Web3ActionButton } from "../advanced/web3action";
-import { Mainnet, DAppProvider, Config, Goerli } from "@usedapp/core";
-import { useClaim, Fuse, Celo } from "@gooddollar/web3sdk-v2";
-import { getDefaultProvider } from "ethers";
-
-const exampleAction = () => {
-  const { isWhitelisted, claimAmount, claimTime, claimCall } = useClaim(10);
-  console.log({ isWhitelisted, claimAmount, claimTime, claimCall });
-
-  const handleClaim = async () => {
-    if (isWhitelisted) {
-      return await claimCall.send();
-    }
-  };
-
-  return handleClaim();
-};
+import { Mainnet, DAppProvider, Config, Goerli, useEthers } from "@usedapp/core";
+import { useClaim, Fuse, Celo, Web3Provider } from "@gooddollar/web3sdk-v2";
+import { getDefaultProvider, ethers } from "ethers";
+import { ExternalProvider } from "@ethersproject/providers";
 
 const config: Config = {
-  networks: [Goerli, Mainnet, Fuse, Celo],
+  networks: [Mainnet, Fuse, Celo, Goerli],
   readOnlyChainId: Mainnet.chainId,
   readOnlyUrls: {
-    [Mainnet.chainId]: getDefaultProvider("mainnet"),
-    [Goerli.chainId]: getDefaultProvider("goerli"),
+    [Mainnet.chainId]: getDefaultProvider("https://mainnet.infura.io/v3/12207372b62941dfb1efd4fe26b95ccc"),
+    [Goerli.chainId]: getDefaultProvider("https://goerli.infura.io/v3/12207372b62941dfb1efd4fe26b95ccc"),
     122: "https://rpc.fuse.io",
     42220: "https://forno.celo.org"
   }
 };
 
 export const W3Wrapper = () => {
+  const ethereum = (window as any).ethereum;
+  const { account } = useEthers();
+  const [provider, setProvider] = useState(new ethers.providers.JsonRpcProvider("https://rpc.fuse.io", "any"));
+  const [accountFound, setAccountFound] = useState(false);
+
+  useEffect(() => {
+    if (!account && !accountFound) {
+      setAccountFound(true);
+      ethereum.request({ method: "eth_requestAccounts" }).then((r: Array<string>) => {
+        if (r.length > 0) {
+          setProvider(new ethers.providers.Web3Provider(ethereum as ExternalProvider, "any"));
+        }
+      });
+    }
+  });
+
+  //todo: should not need two providers, current bug with only web3provider and not able to find connected account
+
   return (
     <DAppProvider config={config}>
-      <Web3ActionButton text={"Claim UBI"} requiredChain={122} doAction={exampleAction} />
+      <Web3Provider env={"fuse"} web3Provider={provider} config={config}>
+        <Web3Action />
+      </Web3Provider>
     </DAppProvider>
   );
+};
+
+const Web3Action = () => {
+  const { account, chainId } = useEthers();
+  //TODO: useClaim is run with RO provider address / probably cause of the two DappProviders used
+  const { isWhitelisted, claimAmount, claimTime, claimCall } = useClaim("everyBlock");
+  const [claimText, setClaimText] = useState<string>("Claim UBI");
+
+  useEffect(() => {
+    // console.log("web3action -- account -->", { account, isWhitelisted });
+  }, [account, chainId]);
+
+  const handleClaim = useCallback(async () => {
+    // console.log("HC isWhitelisted -->", { isWhitelisted });
+    if (isWhitelisted) {
+      // console.log("isWhitelisted");
+      await claimCall.send();
+    }
+  }, [account, chainId, isWhitelisted, claimCall]);
+
+  useEffect(() => {
+    if (claimAmount) {
+      const amount = parseInt(claimAmount.toString());
+      setClaimText(`Claim ${amount}`);
+    }
+  }, [claimAmount]);
+
+  return <Web3ActionButton text={claimText} requiredChain={122} web3Action={handleClaim} />;
 };
 
 export default {
@@ -56,13 +92,5 @@ export default {
         options: ["sm", "md", "lg"]
       }
     }
-  }
-};
-
-export const WebActionButton = {
-  args: {
-    text: "Hello Worlds",
-    colorScheme: "primary",
-    size: "md"
   }
 };
