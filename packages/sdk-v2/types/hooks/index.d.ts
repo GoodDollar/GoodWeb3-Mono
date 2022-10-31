934003c7c3ac0bf6cf73cdab4e8fc7206d0a2000
Lewis B <lewis@ikigaistudios.eu>
2022-10-31 19:00:12 +0100

    add types to gitignore

--- packages/sdk-v2/types/hooks/index.d.ts
+++
@@ -1,8 +0,0 @@
-export { default as useAppState } from './useAppState';
-export { default as useRefreshOrNever } from './useRefreshOrNever';
-export { default as useUpdateEffect } from './useUpdateEffect';
-export * from './useMulticallAtChain';
-export * from './useNativeBalance';
-export * from './usePressOrSwitchChain';
-export * from './useUpdateEffect';
-//# sourceMappingURL=index.d.ts.map
