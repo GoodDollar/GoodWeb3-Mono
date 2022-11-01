c07d7699ebc5ee126b857f2af46f516a044d5e27
John Smith <alexey@gooddollar.org>
2022-11-02 01:36:21 +0200

    add import restart util from GD app

--- packages/sdk-v2/src/hooks/index.ts
+++ packages/sdk-v2/src/hooks/index.ts
@@ -1,7 +1,9 @@
 export { default as useAppState } from './useAppState';
+export { default as useAppRestart } from './useAppRestart';
 export { default as useEthers } from './useEthers';
 export { default as useRefreshOrNever } from './useRefreshOrNever';
 export { default as useUpdateEffect } from './useUpdateEffect';
+export * from './useAppRestart.api';
 export * from './useMulticallAtChain';
 export * from './usePressOrSwitchChain';
 export * from './useUpdateEffect';
