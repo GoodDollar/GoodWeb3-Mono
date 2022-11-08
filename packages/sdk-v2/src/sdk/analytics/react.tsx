adca61bae769e549f71a490f457c24eaaf8c2184
John Smith <alexey@gooddollar.org>
2022-11-08 15:41:36 +0200

    add react provider & hook

---
+++ packages/sdk-v2/src/sdk/analytics/react.tsx
@@ -0,0 +1,57 @@
+import React, { Children, createContext, ReactNode, useCallback, useContext, useEffect, useRef, useState } from 'react'
+import { Analytics, IAnalyticsConfig } from './sdk'
+import { IAbstractProvider, IAnalyticsProvider, IAppProps, IMonitoringProvider, IProvider } from './types'
+
+export interface IAnalyticsContext extends Pick<IAbstractProvider, 'identify'>, IAnalyticsProvider, IMonitoringProvider {}
+
+export interface IAnaliticsProviderProps {
+  config: IAnalyticsConfig;
+  appProps: IAppProps;
+  children?: ReactNode;
+}
+
+export const AnalyticsContext = createContext<IAnalyticsContext>({
+  identify: (identifier: string | number, email?: string, props?: object): void => {},
+  send: (event: string, data?: object): void => {},
+  capture: (exception: Error, fingerprint?: string[], tags?: object, extra?: object): void => {},
+})
+
+export function AnalyticsProvider({ config, appProps }: IAnaliticsProviderProps): ReactNode | null {
+  const [sdk, setSDK] = useState<IProvider | null>(null)
+  const configRef = useRef(config);
+  const appPropsRef = useRef(appProps);
+
+  const send = useCallback((event: string, data?: object): void => {
+    sdk?.send?.(event, data);
+  }, [sdk])
+
+  const capture = useCallback((exception: Error, fingerprint?: string[], tags?: object, extra?: object): void => {
+    sdk?.capture?.(exception, fingerprint, tags, extra);
+  }, [sdk])
+
+  const identify = useCallback((identifier: string | number, email?: string, props?: object): void => {
+    sdk?.identify?.(identifier, email, props);
+  }, [sdk])
+
+  useEffect(() => {
+    const sdk = new Analytics(configRef.current)
+
+    sdk.initialize(appPropsRef.current).then(() => setSDK(sdk))
+  }, [setSDK])
+
+  return !sdk ? null : (
+    <AnalyticsContext.Provider value={{ send, capture, identify }}>
+      {Children}
+    </AnalyticsContext.Provider>
+  )
+}
+
+export function useAnalytics(): IAnalyticsContext {
+  return useContext(AnalyticsContext);
+}
+
+export function useSendAnalytics(): IAnalyticsContext['send'] {
+  const { send } = useAnalytics();
+
+  return send;
+}
