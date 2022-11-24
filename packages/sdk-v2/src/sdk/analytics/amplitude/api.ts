b9d64da2c663e07c875c22b695fba700341210d7
johnsmith-gooddollar <89783679+johnsmith-gooddollar@users.noreply.github.com>
2022-11-24 14:05:10 +0200

    Update api.ts

--- packages/sdk-v2/src/sdk/analytics/amplitude/api.ts
+++ packages/sdk-v2/src/sdk/analytics/amplitude/api.ts
@@ -1,3 +1,24 @@
-import * as amplitude from '@amplitude/analytics-browser';
+import { createInstance, Identify } from '@amplitude/analytics-browser';
 
-export default amplitude;
+class Wrapper {
+  private api = createInstance()
+  readonly Identify = Identify
+
+  async initialize(apiKey: string) {
+    return this.api.init(apiKey);
+  }
+
+  setUserId(id: string): void {
+    this.api.setUserId(id);
+  }
+
+  setUserProperties(props: Record<string, unknown>) {
+    // TODO: implement
+  }
+
+  logEvent(event: string, data: Record<string, unknown>) {
+    this.api.logEvent(event, data);
+  }
+}
+
+export default new Wrapper();
