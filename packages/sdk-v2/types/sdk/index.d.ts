8a914d78ca9cd67ea6f5692661d89a9a90138b90
johnsmith-gooddollar <89783679+johnsmith-gooddollar@users.noreply.github.com>
2022-11-25 17:35:01 +0200

    Delete index.d.ts

--- packages/sdk-v2/types/sdk/index.d.ts
+++
@@ -1,7 +0,0 @@
-export * from './analytics';
-export * from './base';
-export * from './claim';
-export * from './constants';
-export * from './faucet';
-export * from './savings';
-//# sourceMappingURL=index.d.ts.map
