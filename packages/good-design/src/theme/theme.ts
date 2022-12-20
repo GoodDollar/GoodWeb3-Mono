c79b03cacdfd0b78f8e651568fb1ffcfc94fdb65
alt-a1r <a.maslakov@artjoker.team>
2022-12-20 12:45:03 +0200

    revert fonts config

--- packages/good-design/src/theme/theme.ts
+++ packages/good-design/src/theme/theme.ts
@@ -27,6 +27,74 @@ export const theme = extendTheme({
   sizes: {
     "md": "200px",
   },
+  fontConfig: {
+    Montserrat: {
+      100: {
+        normal: "Montserrat-ExtraLight",
+        italic: "Montserrat-ExtraLightItalic",
+      },
+      200: {
+        normal: "Montserrat-ExtraLight",
+        italic: "Montserrat-ExtraLightItalic",
+      },
+      300: {
+        normal: "Montserrat-Light",
+        italic: "Montserrat-LightItalic",
+      },
+      400: {
+        normal: "Montserrat-Thin",
+        italic: "Montserrat-ThinItalic",
+      },
+      500: {
+        normal: "Montserrat-Regular",
+        italic: "Montserrat-RegularItalic",
+      },
+      600: {
+        normal: "Montserrat-Regular",
+        italic: "Montserrat-RegularItalic",
+      },
+      700: {
+        normal: 'Montserrat-Bold',
+        italic: 'Montserrat-BoldItalic',
+      },
+    },
+    Roboto: {
+      100: {
+        normal: "Roboto-Light",
+        italic: "Roboto-LightItalic",
+      },
+      200: {
+        normal: "Roboto-Light",
+        italic: "Roboto-LightItalic",
+      },
+      300: {
+        normal: "Roboto-Light",
+        italic: "Roboto-LightItalic",
+      },
+      400: {
+        normal: "Roboto-Regular",
+        italic: "Roboto-Italic",
+      },
+      500: {
+        normal: "Roboto-Medium",
+      },
+      600: {
+        normal: "Roboto-Medium",
+        italic: "Roboto-MediumItalic",
+      },
+      700: {
+        normal: 'Roboto-Bold',
+      },
+      800: {
+        normal: 'Roboto-Bold',
+        italic: 'Roboto-BoldItalic',
+      },
+      900: {
+        normal: 'Roboto-Bold',
+        italic: 'Roboto-BoldItalic',
+      },
+    },
+  },
   fonts: {
     heading: "Montserrat",
     body: "Montserrat",
