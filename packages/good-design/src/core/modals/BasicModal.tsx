4b7c99f16fa0333bb713853cf2e0c1e3c89ccf97
alt-a1r <a.maslakov@artjoker.team>
2022-12-23 15:23:01 +0200

    fix

--- packages/good-design/src/core/modals/BasicModal.tsx
+++ packages/good-design/src/core/modals/BasicModal.tsx
@@ -37,7 +37,7 @@ const BasicModal: FC<BasicModalProps> = ({
   _body = {},
   _footer = {}
 }) => {
-  const backgroundColor = useColorModeValue("white", "mainDarkContrast");
+  const backgroundColor = useColorModeValue("white:alpha.40", "mainDarkContracts:alpha.40");
 
   const onActionButtonPress = useCallback(() => {
     onAction();
@@ -47,17 +47,17 @@ const BasicModal: FC<BasicModalProps> = ({
   const actionButton = actionText ? <Button onPress={onActionButtonPress}>{actionText}</Button> : <React.Fragment />;
   return (
     /* height 100vh is required so modal always shows in the middle */
-    <NBModal isOpen={modalVisible} onClose={onClose} {..._modal} minH="100vh" >
-      <NBModal.Content bgColor={backgroundColor}>
+    <NBModal isOpen={modalVisible} onClose={onClose} {..._modal} minH="100vh" bgColor={backgroundColor}>
+      <NBModal.Content >
         {hasCloseButton && <NBModal.CloseButton />}
         {!!header && (
-          <NBModal.Header borderBottomWidth={hasTopBorder ? "px" : "0"} {..._header} bgColor={backgroundColor}>
+          <NBModal.Header borderBottomWidth={hasTopBorder ? "px" : "0"} {..._header} >
             {header}
           </NBModal.Header>
         )}
         <NBModal.Body {..._body}>{body}</NBModal.Body>
         {(!!footer || !!closeText || !!actionText) && (
-          <NBModal.Footer borderTopWidth={hasBottomBorder ? "px" : "0"} {..._footer} bgColor={backgroundColor}>
+          <NBModal.Footer borderTopWidth={hasBottomBorder ? "px" : "0"} {..._footer} >
             {footer}
             <Button.Group space={2}>
               {closeText ? (
