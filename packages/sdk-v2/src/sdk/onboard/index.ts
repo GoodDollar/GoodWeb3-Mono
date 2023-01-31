1578af62d12134fb4fe0221f1781f6c87738873b
L03TJ3 <lewis@ikigaistudios.eu>
2023-01-31 08:16:37 +0100

    hide torus button

--- packages/sdk-v2/src/sdk/onboard/index.ts
+++ packages/sdk-v2/src/sdk/onboard/index.ts
@@ -1,2 +1,3 @@
-export * from './modules';
+export * from './modules/customwalletconnect';
+export * from './modules/torus';
 export * from './react';
--- packages/sdk-v2/src/sdk/onboard/react.tsx
+++ packages/sdk-v2/src/sdk/onboard/react.tsx
@@ -1,10 +1,10 @@
 import { InitOptions, OnboardAPI } from "@web3-onboard/core";
-import { init, Web3OnboardProvider } from '@web3-onboard/react';
-import injectedModule from '@web3-onboard/injected-wallets';
-import walletConnectModule from '@web3-onboard/walletconnect'
-import coinbaseWalletModule from '@web3-onboard/coinbase'
+import { init, Web3OnboardProvider } from "@web3-onboard/react";
+import injectedModule from "@web3-onboard/injected-wallets";
+import walletConnectModule from "@web3-onboard/walletconnect";
+import coinbaseWalletModule from "@web3-onboard/coinbase";
 import { useRef } from "react";
-import { torus as torusModule } from './modules/torus'
+import { torus as torusModule } from "./modules/torus";
 import { customWcModule } from "./modules/customwalletconnect";
 import { keys, pickBy } from "lodash";
 
@@ -26,71 +26,74 @@ export interface IOnboardProviderProps {
 
 const injected = injectedModule({
   filter: {
-      ['Binance Smart Wallet']: false,
-      ['MetaMask']: true,
-      ['Coinbase Wallet']: true,
-      ['detected']: true,
-      ['trust']: false,
-      ['opera']: false,
-      ['status']: false,
-      ['alphawallet']: false,
-      ['atoken']: false,
-      ['bitpie']: false,
-      ['blockwallet']: false,
-      ['Brave']: false,
-      ['dcent']: false,
-      ['frame']: false,
-      ['huobiwallet']: false,
-      ['hyperpay']: false,
-      ['imtoken']: false,
-      ['liquality']: false,
-      ['meetone']: false,
-      ['ownbit']: false,
-      ['mykey']: false,
-      ['tokenpocket']: false,
-      ['tp']: false,
-      ['xdefi']: false,
-      ['oneInch']: false,
-      ['tokenary']: false,
-      ['tally']: false,
-  },
-})
+    ["Binance Smart Wallet"]: false,
+    ["MetaMask"]: true,
+    ["Coinbase Wallet"]: true,
+    ["detected"]: true,
+    ["trust"]: false,
+    ["opera"]: false,
+    ["status"]: false,
+    ["alphawallet"]: false,
+    ["atoken"]: false,
+    ["bitpie"]: false,
+    ["blockwallet"]: false,
+    ["Brave"]: false,
+    ["dcent"]: false,
+    ["frame"]: false,
+    ["huobiwallet"]: false,
+    ["hyperpay"]: false,
+    ["imtoken"]: false,
+    ["liquality"]: false,
+    ["meetone"]: false,
+    ["ownbit"]: false,
+    ["mykey"]: false,
+    ["tokenpocket"]: false,
+    ["tp"]: false,
+    ["xdefi"]: false,
+    ["oneInch"]: false,
+    ["tokenary"]: false,
+    ["tally"]: false
+  }
+});
 
 const defaultWc = walletConnectModule({
-  bridge: 'https://bridge.walletconnect.org',
+  bridge: "https://bridge.walletconnect.org",
   qrcodeModalOptions: {
-      mobileLinks: ['rainbow', 'metamask', 'argent', 'trust', 'imtoken', 'pillar'],
-  },
-})
+    mobileLinks: ["rainbow", "metamask", "argent", "trust", "imtoken", "pillar"]
+  }
+});
 
-const coinbaseWalletSdk = coinbaseWalletModule()
+const coinbaseWalletSdk = coinbaseWalletModule();
 const zenGoWc = customWcModule({
-  customLabelFor: 'zengo',
-  bridge: 'https://bridge.walletconnect.org',
+  customLabelFor: "zengo",
+  bridge: "https://bridge.walletconnect.org",
   qrcodeModalOptions: {
-      desktopLinks: ['zengo', 'metamask'],
-      mobileLinks: ['metamask', 'zengo'], // TODO: has to be tested on IOS, android does not show list
-  },
-})
+    desktopLinks: ["zengo", "metamask"],
+    mobileLinks: ["metamask", "zengo"] // TODO: has to be tested on IOS, android does not show list
+  }
+});
 
 const gdWc = customWcModule({
-  customLabelFor: 'gooddollar',
-  bridge: 'https://bridge.walletconnect.org',
-})
+  customLabelFor: "gooddollar",
+  bridge: "https://bridge.walletconnect.org"
+});
 
 const torus = torusModule({
-  buildEnv: 'testing',
-})
+  buildEnv: "testing",
+  showTorusButton: false
+});
 
 const defaultOptions: IOnboardProviderProps["options"] = {
-  chains: [{
-    id: "0xa4ec",
-    token: 'CELO',
-    label: 'CELO Testnet',
-    rpcUrl: "https://alfajores-forno.celo-testnet.org",
-    namespace: "evm",
-  }]
-}
+  chains: [
+    {
+      id: "0xa4ec",
+      token: "CELO",
+      label: "CELO Testnet",
+      rpcUrl: "https://alfajores-forno.celo-testnet.org",
+      namespace: "evm"
+    }
+  ]
+};
 
 const defaultWalletsFlags: IOnboardWallets = {
   torus: true,
@@ -99,8 +102,8 @@ const defaultWalletsFlags: IOnboardWallets = {
   walletconnect: true,
   coinbase: true,
   zengo: true,
-  custom: [],
-}
+  custom: []
+};
 
 const walletsMap: Record<keyof Omit<IOnboardWallets, "custom">, any> = {
   torus,
@@ -108,29 +111,30 @@ const walletsMap: Record<keyof Omit<IOnboardWallets, "custom">, any> = {
   metamask: injected,
   walletconnect: defaultWc,
   coinbase: coinbaseWalletSdk,
-  zengo: zenGoWc,
+  zengo: zenGoWc
 };
 
-export const OnboardProvider = ({ options = defaultOptions, wallets = null, children }: IOnboardProviderProps): JSX.Element => {
+export const OnboardProvider = ({
+  options = defaultOptions,
+  wallets = null,
+  children
+}: IOnboardProviderProps): JSX.Element => {
   const onboardRef = useRef<OnboardAPI>();
 
   // initialise once at first render
-  ;(() => {
+  (() => {
     if (onboardRef.current) {
       return;
     }
-    
-    const { custom = [], ...flags } = { ...defaultWalletsFlags, ...(wallets || {}) };       
+
+    const { custom = [], ...flags } = { ...defaultWalletsFlags, ...(wallets || {}) };
     const selectedWallets = keys(pickBy(flags));
 
     onboardRef.current = init({
       ...options,
-      wallets: [
-        ...selectedWallets.map(key => walletsMap[key]),
-        ...custom
-      ],
-    })
+      wallets: [...selectedWallets.map(key => walletsMap[key]), ...custom]
+    });
   })();
 
   return <Web3OnboardProvider web3Onboard={onboardRef.current}>{children}</Web3OnboardProvider>;
-}
+};
