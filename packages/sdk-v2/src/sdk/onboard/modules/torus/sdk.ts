3d88669d2fbf9b002304c451d0c398623d30d29f
L03TJ3 <lewis@ikigaistudios.eu>
2023-02-15 14:27:51 +0100

    update label name for google button

--- packages/sdk-v2/src/sdk/onboard/modules/torus/sdk.ts
+++ packages/sdk-v2/src/sdk/onboard/modules/torus/sdk.ts
@@ -1,9 +1,9 @@
-import { WalletInit, WalletModule } from '@web3-onboard/common'
-import { default as defaultTorusModule } from '@web3-onboard/torus'
-import { TorusCtorArgs, TorusParams } from '@toruslabs/torus-embed'
-import { getDevice } from '../../../base/utils'
+import { WalletInit, WalletModule } from "@web3-onboard/common";
+import { default as defaultTorusModule } from "@web3-onboard/torus";
+import { TorusCtorArgs, TorusParams } from "@toruslabs/torus-embed";
+import { getDevice } from "../../../base/utils";
 
-type TorusOptions = TorusCtorArgs & TorusParams
+type TorusOptions = TorusCtorArgs & TorusParams;
 
 const GoogleIcon = `
 <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">
@@ -12,18 +12,18 @@ const GoogleIcon = `
   <path d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z" fill="#FBBC05"/>
   <path d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z" fill="#EA4335"/><path d="M1 1h22v22H1z" fill="none"/>
   </svg>
-`
+`;
 
 function torus(options?: TorusOptions): WalletInit {
-  const defaultTorus = defaultTorusModule(options)
+  const defaultTorus = defaultTorusModule(options);
 
   return () => {
     const walletInit = defaultTorus({ device: getDevice() }) as WalletModule;
 
-    walletInit.label = 'Google (Powered by Web3Auth)';
+    walletInit.label = "Google (Web3Auth)";
     walletInit.getIcon = async () => GoogleIcon;
-    return walletInit
-  }
+    return walletInit;
+  };
 }
 
-export default torus
+export default torus;
