9b9eedd9fd1ba883a1551fc5ba4963aaddeba70c
LewisB <lewis@ikigaistudios.eu>
2023-04-03 17:29:00 +0200

    add props for resetState and networks

--- packages/good-design/src/core/web3/KimaModal.tsx
+++ packages/good-design/src/core/web3/KimaModal.tsx
@@ -1,19 +1,26 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useCallback } from "react";
 import { useModal } from "../../hooks/useModal";
 import { Box, Text } from "native-base";
 import { LearnButton } from "../buttons";
 
+type BridgeNetworks = {
+  origin: string;
+  destination: string;
+};
+
 interface KimaModalProps {
   success: boolean | undefined;
+  networks: BridgeNetworks;
+  resetState: any;
   children: any;
 }
 
-const KimaModalCopy = (success: boolean) => ({
+const KimaModalCopy = (success: boolean, networks: BridgeNetworks) => ({
   header: (
     <Box backgroundColor={"white"}>
       <Text color="goodGrey.500" fontSize="sm" fontFamily="subheading">
         {success
-          ? `You have succesfully bridged from NetworkX to NetworkY`
+          ? `You have succesfully bridged from ${networks.origin} to ${networks.destination}`
           : `Something went wrong while trying to bridge your tokens`}
       </Text>
     </Box>
@@ -21,9 +28,9 @@ const KimaModalCopy = (success: boolean) => ({
   body: <LearnButton source="bridging" />
 });
 
-export const KimaModal = ({ success, children }: KimaModalProps) => {
+export const KimaModal = ({ success, networks, resetState, children }: KimaModalProps) => {
   const { Modal, showModal } = useModal();
-  const kimaCopy = KimaModalCopy(success ?? false);
+  const kimaCopy = KimaModalCopy(success ?? false, networks);
 
   useEffect(() => {
     if (success !== undefined) {
@@ -31,9 +38,13 @@ export const KimaModal = ({ success, children }: KimaModalProps) => {
     }
   }, [success, showModal]);
 
+  const onClose = useCallback(() => {
+    resetState();
+  }, [resetState]);
+
   return (
     <React.Fragment>
-      <Modal {...kimaCopy} closeText="x" />
+      <Modal {...kimaCopy} onClose={onClose} closeText="x" />
       {children}
     </React.Fragment>
   );
--- packages/good-design/src/stories/core/web3/KimaModal.stories.tsx
+++ packages/good-design/src/stories/core/web3/KimaModal.stories.tsx
@@ -1,12 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { KimaModal } from "../../../core";
 import { W3Wrapper } from "../../W3Wrapper";
 import { Box } from "native-base";
 
 const KimaModalExample = () => {
   const [success, setSuccess] = useState<boolean | undefined>(undefined);
+
+  const resetState = () => {
+    setSuccess(undefined);
+  };
+
   return (
-    <KimaModal success={success}>
+    <KimaModal success={success} resetState={resetState} networks={{ origin: "Fuse", destination: "Celo" }}>
       <Box w="50%" h="50%" display="flex" flexDir="row" justifyContent="center" alignItems="center">
         <button onClick={() => setSuccess(true)}>Success</button>
         <button onClick={() => setSuccess(false)}>Failed</button>
