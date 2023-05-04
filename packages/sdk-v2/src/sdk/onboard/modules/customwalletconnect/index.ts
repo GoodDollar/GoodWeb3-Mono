b385460a53a3c7a9e2bf53dc2860b3ab462ee17b
LewisB <lewis@gooddollar.org>
2023-05-04 13:54:43 +0200

    Dev version beta.5748adb released

--- packages/sdk-v2/src/sdk/onboard/modules/customwalletconnect/index.ts
+++ packages/sdk-v2/src/sdk/onboard/modules/customwalletconnect/index.ts
@@ -1,4 +1,4 @@
-export { default as customWc2Module } from "./sdkv2";
-export { default as customWcModule } from "./sdk";
-export * from "./icons";
-export * from "./types";
+export { default as customWc2Module } from './sdkv2';
+export { default as customWcModule } from './sdk';
+export * from './icons';
+export * from './types';
