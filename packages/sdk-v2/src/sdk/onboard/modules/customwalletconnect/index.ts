6ea2345e9ce1e36ef4c877bf1a253b49ac091c32
sirpy <hadarr@gmail.com>
2023-05-10 10:56:29 +0300

    fix: index file

--- packages/sdk-v2/src/sdk/onboard/modules/customwalletconnect/index.ts
+++ packages/sdk-v2/src/sdk/onboard/modules/customwalletconnect/index.ts
@@ -1,4 +1,3 @@
-export { default as customWc2Module } from './sdkv2';
-export { default as customWcModule } from './sdk';
-export * from './icons';
-export * from './types';
+export * from "./icons";
+export * from "./sdk";
+export * from "./types";
