94c174caec9a8bec19b1f35bc629f1b248a5589b
LewisB <lewis@gooddollar.org>
2023-06-16 06:49:42 +0200

    fix: also include pendingSignature

--- packages/good-design/src/stories/core/web3/SignTxModal.stories.tsx
+++ packages/good-design/src/stories/core/web3/SignTxModal.stories.tsx
@@ -2,7 +2,7 @@ import React, { useCallback, useEffect, useState } from "react";
 import { SignTxModal } from "../../../core";
 import { W3Wrapper } from "../../W3Wrapper";
 import BaseButton from "../../../core/buttons/BaseButton";
-import { Box, Button, HStack, Center } from "native-base";
+import { Box, HStack, Center, Heading } from "native-base";
 import { useNotifications, useEthers } from "@usedapp/core";
 import { ethers } from "ethers";
 import { JsonRpcSigner } from "@ethersproject/providers";
@@ -58,10 +58,14 @@ const SignTxModalExample = () => {
           </>
         )}
       </Box>
-      <Box>
+      <Box display="flex" justifyContent="center" alignItems="center">
         {notifications.map(notification => {
-          if ("transaction" in notification)
-            return (
+          // this first if check is needed because of a weird type inference
+          if (notification.type === "walletConnected") return;
+          else notification.type.includes("transaction");
+          return (
+            <Box key={notification.id}>
+              <Heading>Notifications</Heading>
               <HStack
                 key={notification.id}
                 space={1}
@@ -84,7 +88,8 @@ const SignTxModalExample = () => {
                   submittedAt: {notification.submittedAt}{" "}
                 </Center>
               </HStack>
-            );
+            </Box>
+          );
         })}
       </Box>
     </SignTxModal>
