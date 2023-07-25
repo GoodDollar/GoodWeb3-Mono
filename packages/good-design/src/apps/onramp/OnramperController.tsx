7f6a69e112a1c8b4a25bae539c9d55db5fc7b5f4
sirpy <hadarr@gmail.com>
2023-07-25 10:01:22 +0300

    wip: buygd

--- packages/good-design/src/apps/onramp/OnramperController.tsx
+++ packages/good-design/src/apps/onramp/OnramperController.tsx
@@ -1,38 +1,93 @@
 import React, { useCallback, useEffect, useRef, useState } from "react";
 import { Onramper } from "./Onramper";
-import { useEthers, useEtherBalance } from "@usedapp/core";
+import { useEthers, useEtherBalance, useTokenBalance, useCall, useCalls } from "@usedapp/core";
 import { WebViewMessageEvent } from "react-native-webview";
+import { useContractFunctionWithDefaultGasFees, useGetContract } from "@gooddollar/web3sdk-v2";
+import { BigNumber } from "ethers";
+import { useModal } from "../../hooks/useModal";
+import { View, Text } from "native-base";
 export const OnramperController = () => {
-  const { account } = useEthers();
-  const [targetWallet] = useState("");
-  const targetWalletRef = useRef("");
-  const balance = useEtherBalance(targetWalletRef.current);
+  const cdai = "";
+  const { account, chainId } = useEthers();
+  const targetGDHelper = useCall(
+    account && {
+      contract: buygdFactory,
+      method: "predict",
+      args: [account]
+    },
+
+    { refresh: "never", chainId }
+  );
+  const celoBalance = useEtherBalance(targetGDHelper?.value);
+  const cdaiBalance = useTokenBalance(cdai, targetGDHelper?.value);
+
+  const buygd = useGetContract("BuyGDHelper", false, "base") as BuyGDHelper;
+  const buygdFactory = useGetContract("BuyGDFactory", false, "base") as BuyGDFactory;
+
+  const { showModal, hideModal, Modal } = useModal();
+  const { send } = useContractFunctionWithDefaultGasFees(buygd, "swap", {
+    transactionName: "Exchange bought tokens to G$"
+  });
+
+  const selfSwap = false;
+
   const [step, setStep] = useState(-1);
 
+  /**
+   * callback to get event from onramper iframe
+   */
   const callback = useCallback((event: WebViewMessageEvent) => {
     if (event.nativeEvent.title === "success") {
-      targetWalletRef.current = targetWallet;
+      //start the stepper
       setStep(0);
     }
   }, []);
 
-  const triggerSwap = async () => {
-    //await tx
-    //setStep(2)
+  const calcUniswapV3MinAmount = async () => {
+    return BigNumber.from(0);
   };
 
-  useEffect(() => {
-    if (account) {
-      //predict(account).then(setTargetWallet)
-    }
-  }, [account]);
-  useEffect(() => {
-    if (balance?.gt(0)) {
+  const triggerSwap = async () => {
+    try {
+      const minAmount = calcUniswapV3MinAmount();
       setStep(1);
+      //user sends swap tx
+      if (selfSwap) {
+        const tx = send(minAmount);
+
+        // after tx sent progress the stepper
+        const res = await tx;
+        if (res?.status !== 1) throw Error("reverted");
+      } else {
+        //or sbackends sends swap tx
+        await fetch(`/swap/${targetGDHelper?.value}`);
+      }
+      // when done set stepper at final step
+      setStep(2);
+    } catch (e) {
+      showModal();
+    }
+  };
+
+  // when the helper contract has some balance we trigger the swap
+  useEffect(() => {
+    if (cdaiBalance?.gt(0) || celoBalance?.gt(0)) {
       triggerSwap().catch(e => {
         console.log(e);
       });
     }
-  }, [balance]);
-  return <Onramper onEvent={callback} targetWallet={account} step={step} />;
+  }, [celoBalance, cdaiBalance]);
+
+  const ErrorModal = () => (
+    <View>
+      <Text>Error</Text>
+    </View>
+  );
+
+  return (
+    <>
+      <Modal body={<ErrorModal />} />
+      <Onramper onEvent={callback} targetWallet={targetGDHelper?.value} step={step} />;
+    </>
+  );
 };
