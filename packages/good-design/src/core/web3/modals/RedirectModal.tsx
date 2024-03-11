8df7cb4e4d00b65a41b6dc758fe7db3df9446dd7
LewisB <lewis@gooddollar.org>
2024-03-11 22:48:47 +0800

    change RedirectModal to not wrap children

--- packages/good-design/src/core/web3/modals/RedirectModal.tsx
+++ packages/good-design/src/core/web3/modals/RedirectModal.tsx
@@ -1,32 +1,27 @@
 import React from "react";
+
 import { BasicStyledModal } from "./BasicStyledModal";
 
 interface RedirectModalProps {
   open: boolean;
   url: string;
   onClose: () => void;
-  children: any;
 }
 
 const RedirectCopy = `By accessing this link you are leaving
 gooddapp.org and are being redirected to a 
 third-party, independent website.`;
 
-export const RedirectModal = ({ open, url, onClose, children }: RedirectModalProps) => {
-  return (
-    <>
-      <BasicStyledModal
-        type="ctaX"
-        extUrl={url}
-        show={open}
-        onClose={onClose}
-        title="Redirect Notice"
-        content={RedirectCopy}
-        withOverlay="dark"
-        buttonText="Go to website"
-        withCloseButton
-      />
-      {children}
-    </>
-  );
-};
+export const RedirectModal = ({ open, url, onClose }: RedirectModalProps) => (
+  <BasicStyledModal
+    type="ctaX"
+    extUrl={url}
+    show={open}
+    onClose={onClose}
+    title="Redirect Notice"
+    content={RedirectCopy}
+    withOverlay="dark"
+    buttonText="Go to website"
+    withCloseButton
+  />
+);
