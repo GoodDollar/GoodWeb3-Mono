1c9d46ba6349f2b4722fcf3a900b8e7f7145fff5
LewisB <lewis@gooddollar.org>
2024-03-11 22:49:51 +0800

    wip: add new claim-success modal

---
+++ packages/good-design/src/core/web3/modals/ClaimSuccessModal.tsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+import { noop } from "lodash";
+import { Text } from "native-base";
+
+import { BasicStyledModal } from "./BasicStyledModal";
+
+const ClaimSuccessContent = () => (
+  <>
+    Why not tell your friends on social media?
+    <Text fontFamily="subheading" fontSize="sm" color="primary">
+      Don't forget to tag use
+    </Text>
+  </>
+);
+
+export const ClaimSuccessModal = ({ open, onClose = noop }: { open: boolean; onClose?: () => void }) => {
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    // the open prop is expected to be only true when transaction state is success.
+    // this resets pretty quickly which is why we cannot use it directly for triggering the modal
+    if (open) {
+      setShow(true);
+    }
+  }, [open]);
+
+  return (
+    <BasicStyledModal
+      type="social"
+      show={show}
+      onClose={onClose}
+      title={`Congrats! \n You claimed \n G$ today`}
+      content={<ClaimSuccessContent />}
+      buttonAction={noop}
+      withOverlay="dark"
+      withCloseButton
+    />
+  );
+};
