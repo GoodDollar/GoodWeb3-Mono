0d1bb391bcbd42a1fa00099122c870a97f35b352
LewisB <lewis@gooddollar.org>
2024-03-12 17:15:11 +0800

    define new db schema using certificates definition

--- packages/sdk-v2/src/sdk/goodid/db/createDb.ts
+++ packages/sdk-v2/src/sdk/goodid/db/createDb.ts
@@ -1,17 +1,20 @@
 import Dexie from "dexie";
 
 /**
- * Schema definition for the credentials collection
- * - `type`: should indicate the type of the credential
- * - Other fields are according to Veramo's Credential schema
+ * Schema definition for the certificates collection
+ * - `id`: incrementing id linked to various verified credentials
+ * Schema definition for the certificates collection (A collection of credentials per issuer)
+ * - `type`: should indicate the types of the credential
+ * - Other fields are according to Veramo's/W3C Credential schema
  */
 
 const schema = {
-  credentials: ",type, credentialSubject.id, issuer.id, issuanceDate"
+  certificates: "++id",
+  certificates_credentials: "++index, certificate_id, credentialSubject_id, issuer_id, type, issuanceDate"
 };
 
-export const createCredentialsDb = () => {
-  const db = new Dexie("GoodIdCredentials", { autoOpen: false });
+export const createCertificatesDb = () => {
+  const db = new Dexie("GoodIdCertificates", { autoOpen: true });
   db.version(1).stores(schema);
   return db;
 };
