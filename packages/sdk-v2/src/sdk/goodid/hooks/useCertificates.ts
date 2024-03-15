23e64cd3fdefdff92b9e3bba377567c534c9641f
LewisB <lewis@gooddollar.org>
2024-03-15 10:09:25 +0800

    apply exact solution

--- packages/sdk-v2/src/sdk/goodid/hooks/useCertificates.ts
+++ packages/sdk-v2/src/sdk/goodid/hooks/useCertificates.ts
@@ -1,28 +1,63 @@
-import { useCallback } from "react";
-import { isEmpty } from "lodash";
-import { CredentialTypes } from "../types";
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { useCallback, useContext, useEffect, useState } from "react";
+import { first } from "lodash";
 
-import type { Certificate } from "../types";
+import { GoodIdContext } from "../../../contexts/goodid/GoodIdContext";
 
-// This expects a single issuer (gd) for the mvp purpose
-export const useCertificates = (db: any) => {
-  const getCertificates: (credentialTypes?: string[]) => Promise<Certificate[] | undefined> = useCallback(
-    async credentialTypes => {
-      const keys = credentialTypes ?? Object.keys(CredentialTypes);
-      return await db.certificates.where("type").anyOf(keys).toArray();
+const cleanupCertificate = ({ primary_idx, type_subject_idx, ...certificate }: any) => certificate;
+
+const queryCertificates = async (db, account, types: string[] | null = null) => {
+  if (!db) {
+    throw new Error("Certificates database not initialized");
+  }
+
+  const accountFilter = `did:ethr:${account}`;
+  const queryField = types ? "type_subject_idx" : "credentialSubject.id";
+  const queryValue = types ? types.map(type => `${accountFilter}_${type}`) : accountFilter;
+  const queryOperator = types ? "anyOfIgnoreCase" : "equalsIgnoreCase";
+
+  return db.certificates.where(queryField)[queryOperator](queryValue).reverse().sortBy("issuanceDate");
+};
+
+// set types to `false` explicitly to avoid loading on mount
+// passsign null or empty value will just load all certs without type filtering
+export const useCertificates = (account: string, types: string[] | false | null = null) => {
+  const [certificates, setCertificates] = useState<string[]>([]);
+  const { db } = useContext(GoodIdContext);
+
+  const getCertificate = useCallback(
+    async type =>
+      queryCertificates(db, account, [type])
+        .then(first) // from lodash
+        .then(cleanupCertificate),
+    [account, db]
+  );
+
+  const storeCertificate = useCallback(
+    async certificate => {
+      const { type, credentialSubject } = certificate;
+      const typeSubjectIdx = type.map(item => `${credentialSubject.id}_${item}`);
+
+      // unfortunately we need an additional manual-filled field for the index
+      // because compound indexes cannot be combined with multi-entry ones
+      // that's IndexedDB limitation (
+      return db.certificates.add({ ...certificate, type_subject_idx: typeSubjectIdx });
     },
     [db]
   );
-
-  const storeCertificate = async (certificate: Certificate) => {
-    const existingCertificate = await getCertificates([certificate.type[1]]);
-    if (!isEmpty(existingCertificate)) return;
-    return await db.certificates.add(certificate);
-  };
-
   const deleteCertificate = async (credentialKeys: string[]) => {
     return await db.certificates.bulkDelete(credentialKeys);
   };
 
-  return { storeCertificate, deleteCertificate, getCertificates };
+  useEffect(() => {
+    if (types === false) {
+      return;
+    }
+
+    void queryCertificates(db, account, types).then(certificates =>
+      setCertificates(certificates.map(cleanupCertificate))
+    );
+  }, [db, account, types]);
+
+  return { certificates, getCertificate, storeCertificate, deleteCertificate };
 };
