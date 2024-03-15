cd6a12eb62bb0fe423d71d3cab90539f9ba070a0
John Smith <alexey@gooddollar.org>
2024-03-16 01:30:21 +0200

    final implementation, updated storybook

--- packages/sdk-v2/src/sdk/goodid/db/index.ts
+++ packages/sdk-v2/src/sdk/goodid/db/index.ts
@@ -1,1 +1,8 @@
+import { Certificate } from "../types";
+
 export * from "./createDb";
+
+export interface CertificateRecord extends Certificate {
+  primary_idx: string
+  type_subject_idx: string[]
+}
