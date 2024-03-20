4071a47d1a5b99d693b2fd3122e31f38168bbcf7
LewisB <lewis@gooddollar.org>
2024-03-20 21:36:37 +0800

    fix: types and story delete flow

--- packages/sdk-v2/src/sdk/goodid/db/index.ts
+++ packages/sdk-v2/src/sdk/goodid/db/index.ts
@@ -3,6 +3,6 @@ import { Certificate } from "../types";
 export * from "./createDb";
 
 export interface CertificateRecord extends Certificate {
-  primary_idx: string
-  type_subject_idx: string[]
+  primary_idx: string;
+  type_subject_idx: string[];
 }
--- packages/sdk-v2/src/stories/goodid/CertificatesExample.stories.tsx
+++ packages/sdk-v2/src/stories/goodid/CertificatesExample.stories.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState, useCallback } from "react";
+import React, { useEffect, useCallback } from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 import { View, Text, Button } from "react-native";
 import { useEthers } from "@usedapp/core";
@@ -8,16 +8,16 @@ import { W3Wrapper } from "../W3Wrapper";
 import { GoodIdContextProvider } from "../../contexts/goodid/GoodIdContext";
 import { useAggregatedCertificates, useCertificates } from "../../sdk";
 
-const { Location, Identity, Age, Gender } = CredentialType
+const { Location, Identity, Age, Gender } = CredentialType;
 
 const GoodIdWrapper = ({ children }) => {
   return <GoodIdContextProvider>{children}</GoodIdContextProvider>;
 };
 
-const mockCertificate = (account: string, typesSet: 'location' | 'basicidentity') => {
+const mockCertificate = (account: string, typesSet: "location" | "basicidentity") => {
   const mock: Certificate = {
     credentialSubject: {
-      id: "", // example. Id will be set on server
+      id: "" // example. Id will be set on server
     },
     issuer: {
       id: "did:key:p3Ls9vx5d7NDeqHwJG42bBxB2kzMEOgXB3tjVPQHqy1fjZrY"
@@ -30,57 +30,61 @@ const mockCertificate = (account: string, typesSet: 'location' | 'basicidentity'
       jwt: "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJ2YyI6eyJAY29udGV4dCI6WyJodHRwczovL3d3dy53My5vcmcvMjAxOC9jcmVkZW50aWFscy92MSJdLCJ0eXBlIjpbIlZlcmlmaWFibGVDcmVkZW50aWFsIl0sImNyZWRlbnRpYWxTdWJqZWN0Ijp7InlvdSI6IlJvY2sifX0sInN1YiI6ImRpZDp3ZWI6ZXhhbXBsZS5jb20iLCJuYmYiOjE3MDgzMzI5OTQsImlzcyI6ImRpZDprZXk6ejZNa3Fuc1BzakRpZnFtaEZLMjlnVmdWN2hqU0FVZlNGN2d1VVFSVHRzM2ViV3Y0In0.ViJUzFSe3l3j7opIgDhgKKx-xtiXP3RpjWckyFsnYiCEm7WOv3WTZAVLkGVNXwAsevJZ6Pg9h1X_nSRytLNJAw"
     }
   };
-  
-  // the format would be: 'did:ethr:<wallet-address WITH 0x>                
-  mock.credentialSubject.id = `did:ethr:${account}`
+
+  // the format would be: 'did:ethr:<wallet-address WITH 0x>
+  mock.credentialSubject.id = `did:ethr:${account}`;
 
   switch (typesSet) {
-    case 'location':
-      mock.type.push(Location)
-      mock.credentialSubject.countryCode = "US"
-      break
-    case 'basicidentity':
-      mock.type.push(Identity, Age, Gender)
-      mock.credentialSubject.unique = true
-      mock.credentialSubject.gender = "Male"
-      mock.credentialSubject.age = { from: 18, to: 20 }
+    case "location":
+      mock.type.push(Location);
+      mock.credentialSubject.countryCode = "US";
+      break;
+    case "basicidentity":
+      mock.type.push(Identity, Age, Gender);
+      mock.credentialSubject.unique = true;
+      mock.credentialSubject.gender = "Male";
+      mock.credentialSubject.age = { from: 18, to: 20 };
   }
 
-  return mock
-}
-
+  return mock;
+};
 
 const CertificatesView = () => {
   const { account } = useEthers();
-  const { storeCertificate, deleteCertificate } = useCertificates(account);
-  const certificates = useAggregatedCertificates(account)
+  const { storeCertificate, deleteCertificate } = useCertificates(account ?? "");
+  const certificates = useAggregatedCertificates(account ?? "");
 
-  const createMockCertificate = useCallback(async (typeset: Parameters<typeof mockCertificate>[1]) => {
-    if (storeCertificate && account) {
-      await storeCertificate(mockCertificate(account, typeset))
-    }
-  }, [storeCertificate, account]);
+  const createMockCertificate = useCallback(
+    async (typeset: Parameters<typeof mockCertificate>[1]) => {
+      if (storeCertificate && account) {
+        await storeCertificate(mockCertificate(account, typeset));
+      }
+    },
+    [storeCertificate, account]
+  );
 
   useEffect(() => {
-    if (certificates?.length) {
-      return
+    if (certificates?.length || !account) {
+      return;
     }
 
     // add mocks on mount if nothing exists
-    (['location', 'basicidentity'] as Parameters<typeof mockCertificate>[1][]).forEach(createMockCertificate)    
+    (["location", "basicidentity"] as Parameters<typeof mockCertificate>[1][]).forEach(createMockCertificate);
+    // eslint-disable-next-line react-hooks-addons/no-unused-deps
   }, [account, storeCertificate, certificates, createMockCertificate]);
 
   return (
     <View>
-      <Button title="Issue location certificate" onPress={() => createMockCertificate('location')} />
-      <Button title="Issue basic identity certificate" onPress={() => createMockCertificate('basicidentity')} />
+      <Button title="Issue location certificate" onPress={() => createMockCertificate("location")} />
+      <Button title="Issue basic identity certificate" onPress={() => createMockCertificate("basicidentity")} />
       <Text>Which credentials do you have verified?</Text>
+
       {certificates?.map(({ id, typeName, key, certificate }) => {
         return (
           <View key={key}>
             <Text style={{ fontWeight: "bold" }}>{typeName}</Text>
             <Text>{JSON.stringify(certificate, null, 2)}</Text>
-            <Button title="Delete" onPress={() => deleteCertificate(id)} />
+            <Button title="Delete" onPress={() => deleteCertificate([id])} />
           </View>
         );
       })}
