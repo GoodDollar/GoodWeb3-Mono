ebf51d4229a208681df98c9b469f3e5ca608d2b9
LewisB <lewis@gooddollar.org>
2024-04-16 15:57:43 +0900

    add: generic imagecard component

--- packages/good-design/src/core/layout/index.ts
+++ packages/good-design/src/core/layout/index.ts
@@ -7,3 +7,4 @@ export { default as BalanceGD } from "./BalanceGD";
 export { default as ActionHeader } from "./ActionHeader";
 export { default as SlideDownTab } from "./SlideDownTab";
 export { default as CentreBox } from "./CentreBox";
+export { default as ImageCard } from "./ImageCard";
