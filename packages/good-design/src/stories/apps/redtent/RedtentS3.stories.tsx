36e0067744a3f40cadb1f174a70ab85b2e56e446
LewisB <lewis@gooddollar.org>
2024-04-18 16:06:19 +0900

    remove log

--- packages/good-design/src/stories/apps/redtent/RedtentS3.stories.tsx
+++ packages/good-design/src/stories/apps/redtent/RedtentS3.stories.tsx
@@ -1,8 +1,7 @@
 import React from "react";
 import { RedtentController } from "../../../apps/redtent/RedtentController";
 import { W3Wrapper } from "../../W3Wrapper";
-import { getDevice } from "@gooddollar/web3sdk-v2";
-console.log({ getDevice });
+
 export default {
   title: "Apps/Redtent Video Upload S3",
   component: props => (
