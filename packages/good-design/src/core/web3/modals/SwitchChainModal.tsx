d746d904050d0eec5944ca8ab227e7e85015901e
LewisB <lewis@gooddollar.org>
2024-04-19 10:24:19 +0900

    refac modal structure

--- packages/good-design/src/core/web3/modals/SwitchChainModal.tsx
+++ packages/good-design/src/core/web3/modals/SwitchChainModal.tsx
@@ -2,9 +2,13 @@ import React, { FC, Fragment, PropsWithChildren, useEffect, useState } from "rea
 import { useConfig } from "@usedapp/core";
 import { useSwitchNetwork } from "@gooddollar/web3sdk-v2";
 import { find } from "lodash";
+import { Text } from "native-base";
 
-import { withTheme } from "../../../theme";
 import BasicStyledModal, { ModalFooterLearn } from "./BasicStyledModal";
+
+const SwitchChainBody = ({ networkName }: { networkName: string | undefined }) => (
+  <Text>To complete this action, switch to {networkName} in your wallet.</Text>
+);
 
 /**
  * A modal to wrap your component or page with and show a modal re-active to switchChain requests
@@ -13,43 +17,41 @@ import BasicStyledModal, { ModalFooterLearn } from "./BasicStyledModal";
  * @param children
  * @returns JSX.Element
  */
-export const SwitchChainModal: FC<PropsWithChildren> = withTheme({ name: "BasicStyledModal" })(
-  ({ children, ...props }) => {
-    const config = useConfig();
-    const [requestedChain, setRequestedChain] = useState(0);
-    const { setOnSwitchNetwork } = useSwitchNetwork();
-    const [show, setShow] = useState(false);
+export const SwitchChainModal: FC<PropsWithChildren> = ({ children, ...props }) => {
+  const config = useConfig();
+  const [requestedChain, setRequestedChain] = useState(0);
+  const { setOnSwitchNetwork } = useSwitchNetwork();
+  const [show, setShow] = useState(false);
 
-    useEffect(() => {
-      if (setOnSwitchNetwork) {
-        setOnSwitchNetwork(() => async (chainId: number, afterSwitch: any) => {
-          if (afterSwitch) {
-            setShow(false);
-          } else {
-            setRequestedChain(chainId);
-            setShow(true);
-          }
-        });
-      }
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [setOnSwitchNetwork, setShow]);
+  useEffect(() => {
+    if (setOnSwitchNetwork) {
+      setOnSwitchNetwork(() => async (chainId: number, afterSwitch: any) => {
+        if (afterSwitch) {
+          setShow(false);
+        } else {
+          setRequestedChain(chainId);
+          setShow(true);
+        }
+      });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [setOnSwitchNetwork, setShow]);
 
-    const networkName = find(config.networks, _ => _.chainId === requestedChain)?.chainName;
+  const networkName = find(config.networks, _ => _.chainId === requestedChain)?.chainName;
 
-    return (
-      <Fragment>
-        <BasicStyledModal
-          {...props}
-          title="Action Required"
-          content={`To complete this action, switch to ${networkName} in your wallet.`}
-          show={show}
-          onClose={() => setShow(false)}
-          type="learn"
-          footer={<ModalFooterLearn source="network" />}
-          withCloseButton
-        />
-        {children}
-      </Fragment>
-    );
-  }
-);
+  return (
+    <Fragment>
+      <BasicStyledModal
+        {...props}
+        title="Action Required"
+        body={<SwitchChainBody networkName={networkName} />}
+        show={show}
+        onClose={() => setShow(false)}
+        type="learn"
+        footer={<ModalFooterLearn source="network" />}
+        withCloseButton
+      />
+      {children}
+    </Fragment>
+  );
+};
