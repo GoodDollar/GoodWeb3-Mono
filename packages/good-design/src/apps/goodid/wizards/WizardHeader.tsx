04c78c8964ab59a82a32b765bac83e0a195e57af
LewisB <lewis@gooddollar.org>
2024-04-19 23:29:48 +0900

    add generic wizard-header

---
+++ packages/good-design/src/apps/goodid/wizards/WizardHeader.tsx
@@ -0,0 +1,54 @@
+import React, { useCallback } from "react";
+import { useWizard } from "react-use-wizard";
+import { TouchableOpacity } from "react-native";
+import { ArrowBackIcon, Text, View } from "native-base";
+
+import { ErrorModal } from "../../../core/web3";
+
+export const WizardHeader = ({
+  onDone,
+  onError,
+  onClose
+}: {
+  onDone: (error?: Error) => Promise<void>;
+  onError: any;
+  onClose?: any;
+}) => {
+  const { isFirstStep, previousStep, isLastStep, goToStep } = useWizard();
+
+  const handleBack = useCallback(() => {
+    if (isFirstStep) {
+      void onDone();
+      return;
+    }
+    previousStep();
+  }, [isFirstStep]);
+
+  if (onError) return <ErrorModal error={onError} onClose={onClose ?? (() => goToStep(0))} overlay="dark" />;
+
+  return (
+    <View
+      bg="primary"
+      justifyContent={"center"}
+      alignItems={"center"}
+      height={12}
+      flexDir={"row"}
+      width={375}
+      paddingLeft={isFirstStep || isLastStep ? 0 : 4}
+      paddingRight={4}
+      mb={6}
+    >
+      <View position={"relative"} display={"inline"} width={15}>
+        <TouchableOpacity onPress={handleBack}>
+          {isLastStep || isFirstStep ? null : <ArrowBackIcon color="white" />}
+        </TouchableOpacity>
+      </View>
+
+      <View flex={"auto"} flexDirection={"row"} justifyContent={"center"}>
+        <Text color="white" fontFamily="subheading" fontSize="sm" fontWeight="500" lineHeight={19}>
+          GoodID
+        </Text>
+      </View>
+    </View>
+  );
+};
