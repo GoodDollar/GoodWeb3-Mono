47348216b4c105b8eb9f71af444b52307e58d6dd
LewisB <lewis@gooddollar.org>
2024-04-24 20:11:14 +0900

    add: updateDataValue to wizard content

--- packages/good-design/src/utils/WizardContext.tsx
+++ packages/good-design/src/utils/WizardContext.tsx
@@ -3,21 +3,38 @@ import React, { useCallback, useState } from "react";
 export type WizardContextValues = {
   data: { [key: string]: any };
   setDataValue: (key: string, value: any) => void;
+  updateDataValue: (baseKey: string, updateKey: string, value: any) => void;
 };
 
 export const WizardContext = React.createContext<WizardContextValues>({
   data: {},
   setDataValue: () => {
     return;
+  },
+  updateDataValue: () => {
+    return;
   }
 });
 
 export const WizardContextProvider = ({ children }: { children: React.ReactNode }) => {
-  const [data, setData] = useState({});
+  const [data, setData] = useState({} as { [key: string]: any });
 
   const setDataValue = useCallback((key: string, value: any) => {
     setData(prevData => ({ ...prevData, [key]: value }));
   }, []);
 
-  return <WizardContext.Provider value={{ data, setDataValue }}>{children}</WizardContext.Provider>;
+  const updateDataValue = useCallback((baseKey: string, updateKey: string, value: any) => {
+    setData(prevData => {
+      const baseValue = prevData[baseKey] || {};
+      return {
+        ...prevData,
+        [baseKey]: {
+          ...baseValue,
+          [updateKey]: value
+        }
+      };
+    });
+  }, []);
+
+  return <WizardContext.Provider value={{ data, setDataValue, updateDataValue }}>{children}</WizardContext.Provider>;
 };
