28b36433415d2d300e84436f263b2f1e8eeb8f55
LewisB <lewis@gooddollar.org>
2024-05-03 17:48:42 +0900

    rename theme

---
+++ packages/good-design/src/apps/goodid/components/theme.ts
@@ -0,0 +1,19 @@
+export const CardRowItem = {
+  baseStyle: {
+    width: "45%",
+    fontStyles: {
+      subHeading: {
+        fontFamily: "subheading",
+        fontWeight: 700,
+        color: "goodGrey.600",
+        fontSize: "sm"
+      },
+      subContent: {
+        fontFamily: "subheading",
+        fontWeight: 400,
+        color: "goodGrey.450",
+        fontSize: "xs"
+      }
+    }
+  }
+};
