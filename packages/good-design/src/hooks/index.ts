a273be8630e064e9519f04d552cdae565aa47d17
LewisB <lewis@gooddollar.org>
2024-05-07 14:05:33 +0900

    moved certificatesmap to sdk-v2

--- packages/good-design/src/hooks/index.ts
+++ packages/good-design/src/hooks/index.ts
@@ -3,4 +3,3 @@ export { default as useScreenSize } from "./useScreenSize";
 export * from "./useWindowFocus";
 export * from "./useModal";
 export * from "./useRedirectNoticeModal";
-export * from "./useCertificatesMap";
