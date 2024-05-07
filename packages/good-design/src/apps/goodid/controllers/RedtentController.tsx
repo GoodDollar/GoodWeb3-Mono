c53298d85b09616e464981690d3234d0a4dd08ae
LewisB <lewis@gooddollar.org>
2024-05-07 14:10:48 +0900

    fix: certificates format expected by /redtent endpoint

--- packages/good-design/src/apps/goodid/controllers/RedtentController.tsx
+++ packages/good-design/src/apps/goodid/controllers/RedtentController.tsx
@@ -1,17 +1,19 @@
 import React from "react";
-import { RedtentWizard, RedTentProps } from "../wizards/RedtentWizard";
-import { uploadToS3, useRegisterRedtent } from "@gooddollar/web3sdk-v2";
+import { uploadToS3, useAggregatedCertificates, useRegisterRedtent } from "@gooddollar/web3sdk-v2";
 
-export const RedtentController = (
-  props: Omit<RedTentProps, "onVideo"> & { account: string; credentials: Array<any> }
-) => {
+import { RedtentWizard, RedTentProps } from "../wizards/RedtentWizard";
+
+export const RedtentController = (props: Omit<RedTentProps, "onVideo"> & { account: string }) => {
   const registerRedtent = useRegisterRedtent();
-  const { account, credentials } = props;
+  const { account } = props;
+  const certificatesList = useAggregatedCertificates(account);
+  const certificates = certificatesList.filter(cert => cert.typeName === "Location" || cert.typeName === "Identity");
+
   const onVideo: RedTentProps["onVideo"] = async (base64, extension) => {
     if (base64) {
       const videoFilename = `${props.account}.${extension}`;
       const s3res = await uploadToS3(base64, videoFilename);
-      const registerRes = await registerRedtent({ account, credentials, videoFilename });
+      const registerRes = await registerRedtent({ account, certificates, videoFilename });
       console.log(s3res, registerRes);
     }
   };
