c77e169894597929896b5d8477c557a16a0dbfb2
LewisB <lewis@gooddollar.org>
2024-06-05 02:06:41 +0900

    wip: update claim controller to handle post-claim

--- packages/good-design/src/apps/ubi/controller/ClaimController.tsx
+++ packages/good-design/src/apps/ubi/controller/ClaimController.tsx
@@ -1,42 +1,57 @@
 import React, { useCallback, useEffect, useState } from "react";
-import { useClaim } from "@gooddollar/web3sdk-v2";
+import { getRecentClaims, SupportedChains, useClaim } from "@gooddollar/web3sdk-v2";
 import { QueryParams, useEthers } from "@usedapp/core";
 import { noop } from "lodash";
 
 import { ClaimWizard } from "../wizard/ClaimWizard";
 import { useFormatClaimTransactions } from "../../../hooks";
-// import { ClaimStats } from "../types";
 
 //todo: make context if to much prop drilling
-export const ClaimController = ({ handleConnect }: { handleConnect?: () => Promise<boolean> }) => {
-  const { account, chainId } = useEthers();
+interface ClaimControllerProps {
+  handleConnect?: () => Promise<boolean>;
+  explorerEndPoints: {
+    [key in keyof typeof SupportedChains]: string;
+  };
+}
+
+export const ClaimController = ({ handleConnect, explorerEndPoints }: ClaimControllerProps) => {
+  const { account, chainId, library } = useEthers();
   const [refreshRate, setRefreshRate] = useState<QueryParams["refresh"]>(12);
   const { claimCall, ...claimStats } = useClaim(refreshRate); // <-- rename to useClaimGd? extend to handle daily-ubi + additional pools?
-  const [claimPools, setClaimPools] = useState<any[]>([]);
+  const [preClaimPools, setClaimPools] = useState<any[]>([]);
+  const [postClaimPools, setPostClaimPools] = useState<any[]>([]);
+  const activeChain = SupportedChains[chainId as number] ?? "CELO";
+  const endpoints = explorerEndPoints[activeChain as keyof typeof SupportedChains];
 
   // const ubiPools = useRegisteredPools(account) <-- return typeof ClaimStats [{ pool: "CELO", contract: celoContract, hasClaimed: claimedCelo, claimAmount}] || []
 
-  const formattedTransactionList = useFormatClaimTransactions(claimPools, chainId);
+  const formattedTransactionList = useFormatClaimTransactions(
+    postClaimPools.length > 0 ? postClaimPools : preClaimPools,
+    chainId
+  );
 
   // should handle claiming based on transactionList
   const onClaim = useCallback(async () => {
     const { send } = claimCall ?? {};
-    if (!send) return false;
+    if (!send) return;
 
     setRefreshRate("everyBlock");
-    const claim = await send(); //todo: needs to handle minipay correctly
-
-    if (!claim) {
-      return false;
-    }
-
-    return true;
+    await send(); //todo: needs to handle minipay correctly
   }, [claimCall]);
 
   useEffect(() => {
-    if (claimPools.length === 0 && claimStats.hasClaimed !== undefined) {
+    if (account && preClaimPools.length === 0 && claimStats.hasClaimed === false) {
       setClaimPools(prev => [claimStats, ...prev]);
     }
+  }, [claimStats]);
+
+  useEffect(() => {
+    void (async () => {
+      if (account && postClaimPools.length === 0 && claimStats.hasClaimed === true) {
+        const claimTransactionList = await getRecentClaims(account, endpoints, library);
+        setPostClaimPools(claimTransactionList);
+      }
+    })();
   }, [claimStats]);
 
   return (
@@ -46,6 +61,7 @@ export const ClaimController = ({ handleConnect }: { handleConnect?: () => Promi
         claimPools: formattedTransactionList,
         chainId,
         onTxDetails: noop,
+        claimStatus: claimCall?.state,
         onClaim,
         handleConnect,
         account
