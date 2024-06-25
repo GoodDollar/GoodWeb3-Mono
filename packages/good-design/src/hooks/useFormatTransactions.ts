6c2fd088b192858db1faba9a2c946da99ed0f222
LewisB <lewis@gooddollar.org>
2024-06-26 00:21:50 +0900

    update: formatTransaction hook to handle both pre/post claim lists

--- packages/good-design/src/hooks/useFormatTransactions.ts
+++ packages/good-design/src/hooks/useFormatTransactions.ts
@@ -4,40 +4,44 @@ import { BigNumber } from "ethers";
 
 import { truncateMiddle } from "../utils";
 
-import { ClaimStats } from "../apps/ubi/types";
-
-export const useFormatClaimTransactions = (transactionList: ClaimStats[], chainId: number | undefined): any[] => {
+export const useFormatClaimTransactions = (pools: any[], chainId: number | undefined): any[] => {
   const { defaultEnv } = useGetEnvChainId();
 
   return useMemo(() => {
-    if (!transactionList || transactionList.length === 0 || !chainId) return [];
+    if (!pools || pools.length === 0 || !chainId) return [];
 
     const formattedTransactions: any = { totalAmount: BigNumber.from("0"), transactionList: [] };
 
-    transactionList.map((transaction: ClaimStats) => {
-      const { address, claimAmount = BigNumber.from("0"), contractName, hasClaimed, date = undefined } = transaction;
+    //todo: fix typings
+    pools?.map((pool: any) => {
+      const [poolName] = Object.keys(pool);
+      const { [poolName]: poolDetail } = pool;
 
-      console.log({ hasClaimed }); // temp for 'no-unused-vars' eslint rule
+      poolDetail.map((transaction: any) => {
+        const { address, claimAmount = BigNumber.from("0"), contractName, hasClaimed, date = undefined } = transaction;
 
-      const tokenValue = G$Amount("G$", claimAmount, chainId, defaultEnv);
-      const trunAddr = truncateMiddle(address, 11);
-      const name = contractName ?? "GoodDollar"; // ?? getContractName(address)
-      const network = SupportedChains[chainId];
-      const displayName = `${name} (${trunAddr})`.trim();
+        console.log({ hasClaimed, date }); // temp for 'no-unused-vars' eslint rule
 
-      formattedTransactions.totalAmount = formattedTransactions.totalAmount.add(claimAmount);
-      formattedTransactions.transactionList.push({
-        token: "G$",
-        tokenValue,
-        displayName,
-        network,
-        date,
-        type: date ? "claim-confirmed" : "claim-start"
+        const tokenValue = G$Amount("G$", claimAmount, chainId, defaultEnv);
+        const trunAddr = truncateMiddle(address, 11);
+        const name = contractName ?? "GoodDollar"; // ?? getContractName(address)
+        const network = SupportedChains[chainId];
+        const displayName = `${name} (${trunAddr})`.trim();
+
+        formattedTransactions.totalAmount = formattedTransactions.totalAmount.add(claimAmount);
+        formattedTransactions.transactionList.push({
+          token: "G$",
+          tokenValue,
+          displayName,
+          network,
+          date,
+          type: date ? "claim-confirmed" : "claim-start"
+        });
       });
     });
 
     formattedTransactions.totalAmount = G$Amount("G$", formattedTransactions.totalAmount, chainId, defaultEnv);
 
     return formattedTransactions;
-  }, [transactionList, chainId]);
+  }, [pools, chainId]);
 };
