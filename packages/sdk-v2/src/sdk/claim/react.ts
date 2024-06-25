6de3940470978181c5837da3eeef7df5af7c899d
LewisB <lewis@gooddollar.org>
2024-06-26 00:20:43 +0900

    add: hook for fetching active claim-pools, hook for claiming from multiple pools in a row

--- packages/sdk-v2/src/sdk/claim/react.ts
+++ packages/sdk-v2/src/sdk/claim/react.ts
@@ -1,10 +1,11 @@
 import { IIdentity, UBIScheme } from "@gooddollar/goodprotocol/types";
 import { ChainId, QueryParams, useCalls, useEthers } from "@usedapp/core";
-import { BigNumber } from "ethers";
+import { BigNumber, Contract } from "ethers";
 import { first } from "lodash";
-import { useState, useEffect, useMemo, useRef } from "react";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import { noop } from "lodash";
 import usePromise from "react-use-promise";
+import GoodCollectiveContracts from "@gooddollar/goodcollective-contracts/releases/deployment.json";
 
 import { AsyncStorage } from "../storage/sdk";
 import { EnvKey } from "../base/sdk";
@@ -14,6 +15,9 @@ import useRefreshOrNever from "../../hooks/useRefreshOrNever";
 import { useGetContract, useGetEnvChainId, useReadOnlySDK, useSDK } from "../base/react";
 import { Envs, SupportedChains, SupportedV2Networks } from "../constants";
 import { useContractFunctionWithDefaultGasFees } from "../base/hooks/useGasFees";
+
+import { getContractsFromClaimPools, getMemberPools, getPoolsDetails } from "./utils/pools";
+import { PoolDetails } from "./types";
 
 export const useFVLink = (chainId?: number) => {
   const { chainId: defaultChainId } = useGetEnvChainId();
@@ -33,6 +37,98 @@ export const useIsAddressVerified = (address: string, env?: EnvKey) => {
   return result;
 };
 
+export const useMultiClaim = (poolsDetails: PoolDetails[]) => {
+  const [contract, setContract] = useState<Contract | undefined>(undefined);
+  const [poolContracts, setPoolContracts] = useState<Contract[]>([]);
+  const [claimedContracts, setClaimedContracts] = useState<Contract[]>([]);
+
+  const { state, send, resetState } = useContractFunctionWithDefaultGasFees(contract, "claim", {
+    transactionName: "Claimed UBI"
+  });
+
+  const nextClaim = useCallback(async () => {
+    if (!contract) return;
+
+    await send();
+
+    setClaimedContracts((prev: Contract[]) => [contract, ...prev]);
+
+    const remainingContracts = poolContracts.find(c => !claimedContracts.includes(c) && c !== contract);
+
+    if (!remainingContracts) {
+      setContract(undefined);
+      return;
+    }
+
+    setContract(remainingContracts);
+  }, [contract, poolContracts]);
+
+  useEffect(() => {
+    if (poolsDetails.length && !poolContracts.length) {
+      const poolContracts = getContractsFromClaimPools(poolsDetails);
+      setPoolContracts(poolContracts);
+    }
+  }, [poolsDetails]);
+
+  useEffect(() => {
+    if (contract) {
+      void nextClaim();
+    }
+
+    return () => resetState();
+  }, [contract]);
+
+  return {
+    poolContracts,
+    claimFlowStatus: {
+      isClaimingDone: claimedContracts.length === poolContracts.length,
+      remainingClaims: poolContracts.length - claimedContracts.length
+    },
+    setContract,
+    transactionState: { state, resetState }
+  };
+};
+
+export const useGetMemberUBIPools = () => {
+  const { account, library, chainId } = useEthers();
+  const [poolsDetails, setPoolsDetails] = useState<PoolDetails[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+  const factory = GoodCollectiveContracts["42220"]?.find(envs => envs.name === "development-celo")?.contracts
+    .UBIPoolFactory;
+  const pool = GoodCollectiveContracts["42220"]?.find(envs => envs.name === "development-celo")?.contracts.UBIPool;
+
+  const fetchPools = useCallback(async () => {
+    setLoading(true);
+    setError(null);
+
+    try {
+      if (!library || !account || !factory || !pool || chainId !== 42220) return [];
+      const poolAddresses = await getMemberPools(account, factory, library);
+
+      if (!poolAddresses || !poolAddresses.length) {
+        setPoolsDetails([]);
+        return;
+      }
+
+      const details = await getPoolsDetails(poolAddresses, pool, library, account);
+
+      setPoolsDetails(details);
+    } catch (err: any) {
+      console.error("Error fetching pools:", err);
+      setError(err.message);
+    } finally {
+      setLoading(false);
+    }
+  }, [account, library, chainId]);
+
+  useEffect(() => {
+    void fetchPools();
+  }, [fetchPools]);
+
+  return useMemo(() => ({ poolsDetails, loading, error }), [poolsDetails, factory, pool, loading, error]);
+};
+
 export const useClaim = (refresh: QueryParams["refresh"] = "never") => {
   const refreshOrNever = useRefreshOrNever(refresh);
   const DAY = 1000 * 60 * 60 * 24;
@@ -41,7 +137,7 @@ export const useClaim = (refresh: QueryParams["refresh"] = "never") => {
 
   const ubi = useGetContract("UBIScheme", true, "claim", chainId) as UBIScheme;
   const identity = useGetContract("Identity", true, "claim", chainId) as IIdentity;
-  const claimCall = useContractFunctionWithDefaultGasFees(ubi, "claim", { transactionName: "Claimed Daily UBI" });
+  // const claimCall = useContractFunctionWithDefaultGasFees(ubi, "claim", { transactionName: "Claimed Daily UBI" });
 
   const results = useCalls(
     [
@@ -84,8 +180,8 @@ export const useClaim = (refresh: QueryParams["refresh"] = "never") => {
     claimAmount: (first(results[3]?.value) as BigNumber) || undefined,
     hasClaimed: (first(results[3]?.value) as BigNumber)?.isZero(),
     claimTime: startRef,
-    claimCall,
     address: ubi?.address,
+    contract: ubi,
     contractName: "GoodDollar"
   };
 };
