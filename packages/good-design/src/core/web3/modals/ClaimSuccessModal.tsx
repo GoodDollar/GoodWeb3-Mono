bc0414b9cfe24442818e41c7159a5cdfcd03b41c
LewisB <lewis@gooddollar.org>
2024-06-28 16:09:39 +0900

    fix: copy space

--- packages/good-design/src/core/web3/modals/ClaimSuccessModal.tsx
+++ packages/good-design/src/core/web3/modals/ClaimSuccessModal.tsx
@@ -9,7 +9,7 @@ const ClaimSuccessContent = () => (
   <>
     Why not tell your friends on social media?
     <Text fontFamily="subheading" fontSize="sm" color="primary">
-      Don't forget to tag use
+      {` Don't forget to tag use`}
     </Text>
   </>
 );
