62d25189a0e865a241993c2a2d9d1841d1b69ec6
LewisB <lewis@gooddollar.org>
2024-07-02 07:40:26 +0900

    add: jumping billy gif for post-claim screen

--- packages/good-design/src/apps/ubi/screens/PostClaim.tsx
+++ packages/good-design/src/apps/ubi/screens/PostClaim.tsx
@@ -3,10 +3,13 @@ import { Center, Spinner, Text, VStack } from "native-base";
 import { isEmpty } from "lodash";
 import { useTimer } from "@gooddollar/web3sdk-v2";
 
+import { Image } from "../../../core/images";
 import { Title } from "../../../core/layout";
 import { TransactionList } from "../components/TransactionStateCard";
 import { useClaimContext } from "../context";
 import { GoodButton } from "../../../core";
+
+import BillyHearts from "../../../assets/gifs/billy-hearts.gif";
 
 const NextClaim = ({ time }: { time: Date }) => {
   const [nextClaim, , setClaimTime] = useTimer(time);
@@ -14,38 +17,52 @@ const NextClaim = ({ time }: { time: Date }) => {
   useEffect(() => setClaimTime(time), [time.toString()]);
 
   return (
-    <VStack space={0}>
-      <Text textAlign="center" fontFamily="subheading" fontWeight="normal" fontSize="sm" color="white">
-        {`Your Next Claim`}
-      </Text>
-      <Center backgroundColor="goodGrey.300">
-        {" "}
-        {/** <-- remove wrapping view, replace with blue-button design */}
+    <VStack alignItems="center">
+      <Image source={BillyHearts} alt="billy-hearts" width="100" height="120" />
+      <VStack space={0} backgroundColor="goodGrey.400" borderRadius="100" w="160" h="160" justifyContent="center">
+        <Text textAlign="center" fontFamily="subheading" fontWeight="normal" fontSize="sm" color="white">
+          {`Your Next Claim`}
+        </Text>
         <Text textAlign="center" fontFamily="heading" fontWeight="700" fontSize="l" color="white">
           {nextClaim}
         </Text>
-      </Center>
+      </VStack>
     </VStack>
   );
 };
 
 export const PostClaim: FC = () => {
-  const { claimPools, claimDetails, claimStatus, claimedAlt, onClaim, onClaimFailed, onTxDetails, switchChain } =
-    useClaimContext();
+  const {
+    claimPools,
+    claimDetails,
+    // claimStatus,
+    claimedAlt,
+    // onClaim,
+    // onClaimFailed,
+    onTxDetails,
+    switchChain
+  } = useClaimContext();
   const { transactionList } = claimPools ?? {};
   const { claimTime, isWhitelisted } = claimDetails;
-  const { status, errorMessage } = claimStatus ?? {};
+  // const { status, errorMessage } = claimStatus ?? {};
   const { hasClaimed, altChain } = claimedAlt ?? {};
 
   // temp: lint no-unused-vars
-  console.log({ status, errorMessage, onClaimFailed, onClaim });
+  // // console.log({ status, errorMessage, onClaimFailed, onClaim });
 
   if ((isWhitelisted as any) === undefined || isEmpty(transactionList) || hasClaimed === undefined)
     return <Spinner variant="page-loader" size="lg" />;
 
   return (
     <VStack justifyContent="center" alignItems="center">
-      <VStack width="100%" space={8} paddingBottom={8} borderBottomWidth={1} borderBottomColor="goodGrey.300">
+      <VStack
+        width="100%"
+        space={8}
+        paddingBottom={8}
+        borderBottomWidth={1}
+        borderBottomColor="goodGrey.300"
+        alignItems="center"
+      >
         <Title variant="title-gdblue" fontSize="xl">
           {`You've claimed today`}
         </Title>
