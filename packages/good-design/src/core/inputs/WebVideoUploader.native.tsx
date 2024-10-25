86e77e15f2e6ecf29812a320099bbd02117942f7
LewisB <lewis@gooddollar.org>
2024-10-25 13:25:06 +0700

    fix: video upload on android

--- packages/good-design/src/core/inputs/WebVideoUploader.native.tsx
+++ packages/good-design/src/core/inputs/WebVideoUploader.native.tsx
@@ -1,6 +1,7 @@
 import { View } from "native-base";
 import React, { useCallback } from "react";
-import { launchCamera } from "react-native-image-picker";
+import { PermissionsAndroid } from "react-native";
+import * as ImagePicker from "react-native-image-picker";
 import RNFS from "react-native-fs";
 import { GoodButton } from "../../core";
 
@@ -11,20 +12,26 @@ type Props = {
 export const WebVideoUploader = ({ onUpload, isLoading }: { onUpload: Props["onDone"]; isLoading: boolean }) => {
   const handleVideoUpload = useCallback(async () => {
     try {
-      const { errorMessage, errorCode, didCancel, assets } = await launchCamera({
-        mediaType: "video",
-        cameraType: "front",
-        durationLimit: 60,
-        videoQuality: "low"
-      });
-      if (assets?.[0]?.uri) {
-        const extension = assets?.[0]?.uri.match(/\.([^.]+)$/)?.[1] || "mp4";
+      const granted = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.CAMERA);
 
-        const base64 = await RNFS.readFile(assets[0].uri, "base64");
-        return onUpload({ base64, extension });
-      }
-      if (!didCancel) {
-        void onUpload(undefined, new Error(`video upload failed: ${errorMessage} ${errorCode}`));
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        const { errorMessage, errorCode, didCancel, assets } = await ImagePicker.launchCamera({
+          mediaType: "video",
+          cameraType: "front",
+          durationLimit: 60,
+          videoQuality: "low"
+        });
+        if (assets?.[0]?.uri) {
+          const extension = assets?.[0]?.uri.match(/\.([^.]+)$/)?.[1] || "mp4";
+
+          const base64 = await RNFS.readFile(assets[0].uri, "base64");
+          return onUpload({ base64, extension });
+        }
+        if (!didCancel) {
+          void onUpload(undefined, new Error(`video upload failed: ${errorMessage} ${errorCode}`));
+        }
+      } else {
+        throw new Error("Camera permission denied. You need to manually give permissions in your app settings.");
       }
     } catch (e) {
       void onUpload(undefined, e as Error);
