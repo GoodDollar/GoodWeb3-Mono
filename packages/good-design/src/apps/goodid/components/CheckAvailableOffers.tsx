f54caf6a4d77a5368742ce979c0078876e3779cd
sirpy <hadar@gooddollar.org>
2025-01-31 12:29:19 +0200

    206 dynamic pool details (#218)
    * add: check if ubi pool is available in terms of max members
    
    * add: dynamic pool data
    
    * fix: minTopping returns a number
    
    * copy for claim frequency
    
    * fix: maxMembers field, fix: loading flow of offer
    
    * bump goodcollective sdks
    
    ---------
    
    Co-authored-by: LewisB <lewis@gooddollar.org>

--- packages/good-design/src/apps/goodid/components/CheckAvailableOffers.tsx
+++ packages/good-design/src/apps/goodid/components/CheckAvailableOffers.tsx
@@ -1,11 +1,11 @@
 import React, { FC, useEffect, useState } from "react";
-import { useCheckAvailableOffers } from "@gooddollar/web3sdk-v2";
+import { PoolCriteria, useCheckAvailableOffers, useGetUBIPoolsDetails } from "@gooddollar/web3sdk-v2";
 import { Spinner } from "native-base";
-import { isEmpty, isNull } from "lodash";
 
 import { RedtentController } from "../controllers/RedtentController";
 import { useClaimContext } from "../../ubi";
 
+export type UBIPoolOffer = PoolCriteria & { claimAmount?: string; claimDayFrequency?: string };
 export interface CheckAvailableOffersProps {
   account: string;
   withNavBar: boolean;
@@ -33,6 +33,14 @@ const redtentOffer = [
     Gender: "Female"
   }
 ];
+const redtentProdPools: { [key: string]: string } = {
+  NG: "0xDd1c12f197E6D1E2FBA15487AaAE500eF6e07BCA",
+  CO: "0x0d43131f1577310D6349bAF9D6Da4fC1Cd39764C"
+};
+const redtentDevPools: { [key: string]: string } = {
+  NG: "0x77253761353271813c1aca275de8eec768b217c5",
+  CO: "0x627dbf00ce1a54067f5a34d6596a217a029c1532"
+};
 
 const CheckAvailableOffers: FC<CheckAvailableOffersProps> = ({
   account,
@@ -41,31 +49,54 @@ const CheckAvailableOffers: FC<CheckAvailableOffersProps> = ({
   onDone,
   onError
 }) => {
+  const redtentPools = isDev ? redtentDevPools : redtentProdPools;
   const { activePoolAddresses, poolContracts } = useClaimContext();
+  const poolsDetails = useGetUBIPoolsDetails(Object.values(redtentPools));
   const availableOffers = useCheckAvailableOffers({ account, pools: redtentOffer, isDev, onDone });
-  const [alreadyMember, setAlreadyMember] = useState<boolean | undefined>(undefined);
+  const [hasOffer, setHasOffer] = useState([] as UBIPoolOffer[]);
 
   useEffect(() => {
     const isMember = poolContracts?.some(pool =>
       Object.values(activePoolAddresses).includes(pool.address.toLowerCase())
     );
-    setAlreadyMember(isMember);
 
     if (availableOffers === false || availableOffers?.length === 0 || isMember) {
       void onDone?.(true);
+      return;
     }
-  }, [availableOffers, activePoolAddresses, poolContracts]);
+
+    if (availableOffers && poolsDetails) {
+      const offers = availableOffers
+        .map(offer => {
+          const pool = redtentPools[offer.Location?.countryCode || ""];
+          const poolDetails = poolsDetails.find(_ => _.contract.toLowerCase() === pool.toLowerCase());
+
+          const isAvailable =
+            poolDetails?.ubiSettings.onlyMembers === false ||
+            Number(poolDetails?.ubiSettings.maxMembers) >= Number(poolDetails?.status.membersCount);
+
+          if (isAvailable) {
+            return {
+              ...offer,
+              claimAmount: poolDetails?.nextClaimAmount as string,
+              claimDayFrequency: poolDetails?.ubiSettings.claimPeriodDays as string
+            } as UBIPoolOffer;
+          }
+          return undefined;
+        })
+        .filter(_ => _ !== undefined) as UBIPoolOffer[];
+
+      if (offers.length > 0) {
+        setHasOffer(offers);
+        return;
+      }
+      void onDone?.(true);
+    }
+  }, [availableOffers, /*used*/ activePoolAddresses, /*used*/ poolContracts, poolsDetails]);
 
   // If isNull means we are still waiting for the availableOffers to be fetched
   // else we are just waiting on onDone to handle the next step / navigation
-  if (
-    isNull(availableOffers) ||
-    availableOffers === false ||
-    availableOffers?.length === 0 ||
-    alreadyMember !== false ||
-    isEmpty(poolContracts)
-  )
-    return <Spinner variant="page-loader" size="lg" />;
+  if (!hasOffer || hasOffer.length === 0) return <Spinner variant="page-loader" size="lg" />;
 
   return (
     /* todo-next: Currently hardcoded for redtent campaign, this should handle showing list of offers. needs design */
@@ -73,7 +104,7 @@ const CheckAvailableOffers: FC<CheckAvailableOffersProps> = ({
       {...{
         withNavBar,
         account,
-        availableOffers,
+        availableOffers: hasOffer,
         onDone,
         onError
       }}
