30b94836f8f8f90da98c5d59678442958d6b375c
Sofiane <saidbensliman@proton.me>
2025-03-01 16:02:43 +0000

    Setup notices, add callback for estimatfee,..

--- packages/good-design/src/stories/apps/MPB/MPB.stories.tsx
+++ packages/good-design/src/stories/apps/MPB/MPB.stories.tsx
@@ -5,33 +5,41 @@ import MPB from "../../../apps/MBP/MPB";
 export default {
   title: "Components/MPB",
   component: MPB
-  /*argTypes: {
-    srcNetwork: {
-      control: {
-        type: "inline-radio",
-        options: ["Ethereum", "Celo", "Fuse"]
-      }
-    },
-    dstNetwork: {
-      control: {
-        type: "inline-radio",
-        options: ["Ethereum", "Celo", "Fuse"]
-      }
-    },
-    provider: {
-      control: {
-        type: "inline-radio",
-        options: ["Axelar", "LayerZero"]
-      }
-    }
-  }*/
 };
 
 const Template = args => <MPB {...args} />;
 
-export const Primary = Template.bind({});
-Primary.args = {
+export const MicroBridgeSuccess = Template.bind({});
+MicroBridgeSuccess.args = {
   ethereumBalance: 5000654.454,
   celoBalance: 54654.546,
-  fuseBalance: 54645.578
+  fuseBalance: 54645.578,
+  useCanBridge: (chain: string, amountWei: string) => ({
+    isValid: true,
+    reason: ""
+  }),
+  onBridge: async () => ({
+    success: true,
+    txHash: "0xbridge",
+    relayPromise: Promise.resolve({ txHash: "0xrelay", success: true })
+  }),
+  relayStatus: { chainId: 42220, status: "Success", transaction: { hash: "0xrelay" } },
+  bridgeStatus: { chainId: 122, status: "Success", transaction: { hash: "0xbridge" } }
+};
+export const MicroBridgeFail = Template.bind({});
+MicroBridgeFail.args = {
+  ethereumBalance: 5000654.454,
+  celoBalance: 54654.546,
+  fuseBalance: 54645.578,
+  useCanBridge: (chain: string, amountWei: string) => ({
+    isValid: false,
+    reason: "Insuficent funds to cover bridging fee"
+  }),
+  onBridge: async () => ({
+    success: false,
+    txHash: "",
+    relayPromise: Promise.resolve({ txHash: "0xrelay", success: true })
+  }),
+  relayStatus: undefined,
+  bridgeStatus: undefined
 };
