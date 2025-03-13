3d7568f577cab19b826fea2f790ae8d1087938a1
sirpy <hadar@gooddollar.org>
2025-03-13 14:42:54 +0200

    fix: wip microbridge stories

--- packages/good-design/src/stories/apps/bridge/MicroBridge.stories.tsx
+++ packages/good-design/src/stories/apps/bridge/MicroBridge.stories.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-vars  */
 /* eslint-disable @typescript-eslint/no-empty-function  */
-import React from "react";
+import * as React from "react";
 import { BridgeWizard } from "../../../apps/bridge/wizard/BridgeWizard";
 import { W3Wrapper } from "../../W3Wrapper";
 
@@ -31,7 +31,8 @@ export const MicroBridgeStart = {
       relayPromise: Promise.resolve({ txHash: "0xrelay", success: true })
     }),
     relayStatus: undefined,
-    bridgeStatus: undefined
+    bridgeStatus: undefined,
+    originChain: ["celo", () => undefined]
   }
 };
 
@@ -46,7 +47,8 @@ export const MicroBridgeSuccess = {
     }),
     selfRelayStatus: { status: "Fail" },
     bridgeStatus: { chainId: 122, status: "Success", transaction: { hash: "0xbridge" } },
-    relayStatus: { chainId: 42220, status: "Success", transaction: { hash: "0xrelay" } }
+    relayStatus: { chainId: 42220, status: "Success", transaction: { hash: "0xrelay" } },
+    originChain: "celo"
   }
 };
 export const MicroBridgeError = {
@@ -55,7 +57,8 @@ export const MicroBridgeError = {
     useCanBridge: (chain: string, amountWei: string) => ({ isValid: true, reason: "" }),
     onBridge: async () => ({ success: false, txHash: "0xbridgeerror" }),
     selfRelayStatus: { status: "None" },
-    bridgeStatus: { chainId: 42220, status: "Fail", transaction: { hash: "0xbridgeerror" } }
+    bridgeStatus: { chainId: 42220, status: "Fail", transaction: { hash: "0xbridgeerror" } },
+    originChain: "celo"
   }
 };
 export const MicroRelayError = {
@@ -68,7 +71,8 @@ export const MicroRelayError = {
     }),
     bridgeStatus: { chainId: 122, status: "Success", transaction: { hash: "0xbridge" } },
     selfRelayStatus: { status: "Fail" },
-    relayStatus: { chainId: 42220, status: "Fail", transaction: { hash: "0xrelayerror" } }
+    relayStatus: { chainId: 42220, status: "Fail", transaction: { hash: "0xrelayerror" } },
+    originChain: "celo"
   }
 };
 export const MicroBridgeWaiting = {
@@ -76,7 +80,8 @@ export const MicroBridgeWaiting = {
     useBalanceHook: (chain: string) => (chain === "fuse" ? 100 : 200),
     useCanBridge: (chain: string, amountWei: string) => ({ isValid: true, reason: "" }),
     onBridge: async () => new Promise(() => {}),
-    bridgeStatus: { status: "Mining" }
+    bridgeStatus: { status: "Mining" },
+    originChain: "celo"
   }
 };
 export const MicroBridgeWaitingRelay = {
@@ -89,6 +94,7 @@ export const MicroBridgeWaitingRelay = {
       transaction: { hash: "0xbridge353255353253253253253532535353532532535" },
       chainId: 122
     },
-    relayStatus: { status: "Mining" }
+    relayStatus: { status: "Mining" },
+    originChain: "celo"
   }
 };
