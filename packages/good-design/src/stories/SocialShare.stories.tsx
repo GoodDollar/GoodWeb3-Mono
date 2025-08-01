b041e47928f5bb9dae8f4529bedb9837ecf05cc2
peter Benjamin ani <benjaminparish6@gmail.com>
2025-08-01 04:27:11 +0100

    fix: update arrow-down color to #7A88A5 and enable Storybook controls, all pr changes fixed

--- packages/good-design/src/stories/SocialShare.stories.tsx
+++ packages/good-design/src/stories/SocialShare.stories.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import * as React from "react";
 import { VStack, Text } from "native-base";
 import { SocialShareBar } from "../advanced/socialshare";
 
@@ -11,56 +11,83 @@ export default {
         component: "Social share component for sharing content on social media platforms"
       }
     }
+  },
+  argTypes: {
+    message: {
+      control: { type: "text" },
+      description: "The message to share on social media",
+      defaultValue: "I just did my first claim(s) of G$ this week!"
+    },
+    url: {
+      control: { type: "text" },
+      description: "The URL to share",
+      defaultValue: "https://gooddollar.org"
+    }
   }
 };
 
-export const Default = () => (
+export const Default = args => (
   <VStack space={4} p={4} bg="white" borderRadius="md">
     <Text fontSize="lg" fontWeight="bold" mb={2}>
       Social Share Component
     </Text>
-    <SocialShareBar message="I just did my first claim(s) of G$ this week!" url="https://gooddollar.org" />
+    <SocialShareBar {...args} />
   </VStack>
 );
 
-export const CustomMessage = () => (
+export const CustomMessage = args => (
   <VStack space={4} p={4} bg="white" borderRadius="md">
     <Text fontSize="lg" fontWeight="bold" mb={2}>
       Custom Message
     </Text>
-    <SocialShareBar
-      message="Check out GoodDollar - the future of universal basic income!"
-      url="https://gooddollar.org"
-    />
+    <SocialShareBar {...args} />
   </VStack>
 );
 
-export const CustomURL = () => (
+CustomMessage.args = {
+  message: "Check out GoodDollar - the future of universal basic income!",
+  url: "https://gooddollar.org"
+};
+
+export const CustomURL = args => (
   <VStack space={4} p={4} bg="white" borderRadius="md">
     <Text fontSize="lg" fontWeight="bold" mb={2}>
       Custom URL
     </Text>
-    <SocialShareBar message="I just did my first claim(s) of G$ this week!" url="https://app.gooddollar.org" />
+    <SocialShareBar {...args} />
   </VStack>
 );
 
-export const LongMessage = () => (
+CustomURL.args = {
+  message: "I just did my first claim(s) of G$ this week!",
+  url: "https://app.gooddollar.org"
+};
+
+export const LongMessage = args => (
   <VStack space={4} p={4} bg="white" borderRadius="md">
     <Text fontSize="lg" fontWeight="bold" mb={2}>
       Long Message
     </Text>
-    <SocialShareBar
-      message="I just claimed my first GoodDollar tokens today! This is an amazing project that's working towards universal basic income through blockchain technology. Everyone should check it out!"
-      url="https://gooddollar.org"
-    />
+    <SocialShareBar {...args} />
   </VStack>
 );
 
-export const DarkBackground = () => (
+LongMessage.args = {
+  message:
+    "I just claimed my first GoodDollar tokens today! This is an amazing project that's working towards universal basic income through blockchain technology. Everyone should check it out!",
+  url: "https://gooddollar.org"
+};
+
+export const DarkBackground = args => (
   <VStack space={4} p={4} bg="gray.800" borderRadius="md">
     <Text fontSize="lg" fontWeight="bold" mb={2} color="white">
       Dark Background
     </Text>
-    <SocialShareBar message="I just did my first claim(s) of G$ this week!" url="https://gooddollar.org" />
+    <SocialShareBar {...args} />
   </VStack>
 );
+
+DarkBackground.args = {
+  message: "I just did my first claim(s) of G$ this week!",
+  url: "https://gooddollar.org"
+};
