9d755c6af8025228e21cd51e6a4e2ef1d63e6f00
sirpy <hadar@gooddollar.org>
2026-04-05 16:13:12 +0300

    add: new gas defaults

--- packages/sdk-v2/src/sdk/base/hooks/useGasFees.ts
+++ packages/sdk-v2/src/sdk/base/hooks/useGasFees.ts
@@ -1,7 +1,7 @@
 import { useContractFunction, useEthers } from "@usedapp/core";
-import { BigNumber } from "ethers";
-import { useCallback, useMemo } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { pickBy } from "lodash";
+import { FeeData } from "@ethersproject/providers";
 import {
   ContractFunctionNames,
   Falsy,
@@ -9,21 +9,26 @@ import {
   TransactionOptions,
   TypedContract
 } from "@usedapp/core/dist/esm/src/model";
+import { BigNumber } from "ethers";
 
 // force gasPrice for fuse/celo
 export const useGasFees = () => {
-  const { chainId } = useEthers();
-  const gasPrice = useMemo(() => {
-    //force gas price for celo and ethereum
-    switch (chainId) {
-      case 122:
-        return BigNumber.from(11e9);
-      case 42220:
-        return BigNumber.from(25.001e9);
-    }
-  }, [chainId]);
+  const { library } = useEthers();
+  const [fees, setFees] = useState<FeeData>({
+    gasPrice: BigNumber.from(30e9),
+    maxFeePerGas: BigNumber.from(30e9),
+    maxPriorityFeePerGas: BigNumber.from(1e8),
+    lastBaseFeePerGas: BigNumber.from(30e9)
+  } as FeeData);
 
-  return pickBy({ gasPrice }, _ => _ != null);
+  useEffect(() => {
+    if (library) {
+      void library.getFeeData().then(fees => {
+        setFees(fees);
+      });
+    }
+  }, [library]);
+  return fees;
 };
 
 // wrap usedapp useContractFunction with default gas price for fuse/celo
@@ -32,7 +37,6 @@ export function useContractFunctionWithDefaultGasFees<T extends TypedContract, F
   functionName: FN,
   options?: TransactionOptions
 ) {
-  const { chainId } = useEthers();
   const { send, ...rest } = useContractFunction(contract, functionName, options);
   const gasFees = useGasFees();
   const newSend = useCallback(
@@ -42,19 +46,10 @@ export function useContractFunctionWithDefaultGasFees<T extends TypedContract, F
         const hasOpts = args.length > numberOfArgs;
         const opts = hasOpts ? args[args.length - 1] : {};
         const modifiedArgs = hasOpts ? args.slice(0, args.length - 1) : args;
-
+        // put default gas fees. use gasPrice value instead of maxFeePerGas because maxFeePerGas uses a very large buffer.
         modifiedArgs.push(
           pickBy(
-            "maxFeePerGas" in opts
-              ? {
-                  ...opts,
-                  ...(chainId === 42220
-                    ? { maxFeePerGas: 25.001e9, maxPriorityFeePerGas: 1e8 }
-                    : chainId === 50
-                    ? { maxFeePerGas: 12.5e9 }
-                    : {})
-                }
-              : { ...gasFees, ...opts },
+            { ...{ maxFeePerGas: gasFees?.gasPrice, maxPriorityFeePerGas: gasFees?.maxPriorityFeePerGas }, ...opts },
             _ => _ != null
           )
         );
